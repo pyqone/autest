f5d6e360b78f0aafa618ad443fe53e7c2b1d8468
彭宇琦 <465615774@qq.com>
2020-03-16 20:06:25 +0800

    完成数字限制用例生成以及对长度限制的单元测试

--- src/main/java/pres/auxiliary/work/n/tcase/InformationCase.java
+++ src/main/java/pres/auxiliary/work/n/tcase/InformationCase.java
@@ -42,6 +42,15 @@ public class InformationCase extends Case {
 	public static final String FAIL_EXCEPT_TEXT_END = WordType.FAIL_EXCEPT_TEXT_END.getName();
 	
 	/**
+	 * 整形最小值，用于数字限制型文本框
+	 */
+	public static final int MIN_NUMBER = Integer.MIN_VALUE;
+	/**
+	 * 整形最大值，用于数字限制型文本框
+	 */
+	public static final int MAX_NUMBER = Integer.MAX_VALUE;
+	
+	/**
 	 * 通过测试用例模板库的xml配置文件来构造InformationCase对象
 	 * @param configXmlFile 用例模板库的xml文件对象
 	 */
@@ -51,20 +60,22 @@ public class InformationCase extends Case {
 	
 	@Override
 	public void setReplaceWord(String word, String text) {
+		String sign = "，";
+		
 		//由于预期前后文是直接在文本上拼接，故为保证语句通顺，则自动为其添加
 		//若传入的参数为成功或失败预期的前文时，则在文本后加上逗号
 		if (SUCCESS_EXCEPT_TEXT_START.equals(word) || FAIL_EXCEPT_TEXT_START.equals(word)) {
 			//判断文本最后一个字符是否为逗号，若不是逗号，则将逗号拼接上
-			if (text.lastIndexOf("，") != text.length() - 1) {
-				text += "，";
+			if (text.lastIndexOf(sign) != text.length() - 1) {
+				text += sign;
 			}
 		}
 		
 		//若传入的参数为成功或失败预期的后文时，则在文本前加上逗号
 		if (SUCCESS_EXCEPT_TEXT_END.equals(word) || FAIL_EXCEPT_TEXT_END.equals(word)) {
 			//判断文本第一个字符是否为逗号，若不是逗号，则将逗号拼接上
-			if (text.indexOf("，") != 0) {
-				text = "，" + text;
+			if (text.indexOf(sign) != 0) {
+				text = sign + text;
 			}
 		}
 		
@@ -138,43 +149,26 @@ public class InformationCase extends Case {
 	 * @param name 文本框名称（控件名称）
 	 * @param isMust 是否必填
 	 * @param isRepeat 是否可以与存在的内容重复
+	 * @param isClear 是否有按钮可以清空文本框
 	 * @param inputRuleTypes 输入限制（{@link InputRuleType}枚举类）
 	 * @return 类本身
 	 */
-	public Case addBasicTextboxCase(String name, boolean isMust, boolean isRepeat, InputRuleType... inputRuleTypes) {
+	public Case addBasicTextboxCase(String name, boolean isMust, boolean isRepeat, boolean isClear, InputRuleType... inputRuleTypes) {
 		//清空字段的内容
 		clearFieldText();
 		// 存储case标签的name属性内容
 		String caseName = "addTextboxCase";
 		//添加控件名称
 		wordMap.put(WordType.CONTROL_NAME.getName(), name);
-		//转换输入限制为集合类型
-		List<InputRuleType> inputRules = Arrays.asList(inputRuleTypes);
 		
 		//存储标题信息
 		addFieldText(LabelType.TITLE, getLabelText(caseName, LabelType.TITLE, "1"));
 		
+		//----------------------------------------------------------------
 		//存储步骤与预期信息
-		//不填写或只输入空格
-		addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "1"));
-		//根据是否必填来判断填入成功或失败预期
-		addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, isMust ? "2" : "1"));
+		textboxCommonCase(caseName, isMust, isRepeat, isClear, inputRuleTypes);
 		
-		//填写特殊字符或HTML代码
-		addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "2"));
-		//根据是否存在输入
-		addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, inputRules.size() == 0 || inputRules.contains(InputRuleType.SPE) ? "1" : "2"));
-		
-		//输入非限制的字符
-		if (inputRules.size() != 0) {
-			String inputRuleText = "";
-			for (InputRuleType inputRule : inputRules) {
-				inputRuleText += (inputRule.getName() + "、");
-			}
-			wordMap.put(WordType.INPUT_RULE.getName(), inputRuleText.substring(0, inputRuleText.length() - 1));
-			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "3"));
-			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
-		}
+		//----------------------------------------------------------------
 		
 		//填写存在的内容
 		addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "12"));
@@ -200,27 +194,195 @@ public class InformationCase extends Case {
 	 * @param name 文本框名称（控件名称）
 	 * @param isMust 是否必填
 	 * @param isRepeat 是否可以与存在的内容重复
+	 * @param isClear 是否有按钮可以清空文本框
 	 * @param minLen 最小输入长度限制
 	 * @param maxLen 最大输入长度限制
 	 * @param inputRuleTypes 输入限制（{@link InputRuleType}枚举类）
 	 * @return 类本身
 	 * @throws CaseContentException 当限制参数传入有误时抛出
 	 */
-	public Case addLengthRuleTextboxCase(String name, boolean isMust, boolean isRepeat, int minLen, int maxLen, InputRuleType... inputRuleTypes) {
+	public Case addLengthRuleTextboxCase(String name, boolean isMust, boolean isRepeat, boolean isClear, int minLen, int maxLen, InputRuleType... inputRuleTypes) {
 		//清空字段的内容
 		clearFieldText();
 		// 存储case标签的name属性内容
 		String caseName = "addTextboxCase";
 		//添加控件名称
 		wordMap.put(WordType.CONTROL_NAME.getName(), name);
-		//转换输入限制为集合类型
-		List<InputRuleType> inputRules = Arrays.asList(inputRuleTypes);
 		
 		//存储标题信息
 		addFieldText(LabelType.TITLE, getLabelText(caseName, LabelType.TITLE, "1"));
 		
 		//----------------------------------------------------------------------
 		//存储步骤与预期信息
+		//添加通用用例
+		textboxCommonCase(caseName, isMust, isRepeat, isClear, inputRuleTypes);
+		
+		//添加输入长度限制相关的测试用例
+		//判断传入的限制参数是否有小于0的参数，参数小于0则直接转换为0
+		minLen = minLen < 0 ? 0 : minLen;
+		maxLen = maxLen < 0 ? 0 : maxLen;
+		//判断长度是否都为0，若都为0，则抛出异常
+		if (minLen == 0 && maxLen == 0) {
+			throw new CaseContentException("长度限制不能全为0");
+		} else if (minLen != 0 && maxLen == 0) {
+			//存储最小输入限制
+			wordMap.put(WordType.INPUT_MIN_LENGTH.getName(), String.valueOf(minLen));
+			
+			//小于最小限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "4"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
+			//等于最小限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "5"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
+		} else if ((minLen == 0 && maxLen != 0) || minLen == maxLen) {
+			//存储最大输入限制
+			wordMap.put(WordType.INPUT_MAX_LENGTH.getName(), String.valueOf(maxLen));
+			//大于最大限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "6"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
+			//等于最大限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "7"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
+		} else {
+			//若都不为0，则将相应的内容进行存储，且判断最大长度与最小长度是否传反，若传反，则调换顺序
+			wordMap.put(WordType.INPUT_MIN_LENGTH.getName(), String.valueOf(minLen < maxLen ? minLen : maxLen));
+			wordMap.put(WordType.INPUT_MAX_LENGTH.getName(), String.valueOf(minLen < maxLen ? maxLen : minLen));
+			
+			//小于最小限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "4"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
+			//等于最小限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "5"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
+			//大于最大限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "6"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
+			//等于最大限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "7"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
+		}
+		//----------------------------------------------------------------------
+		
+		//存储前置条件信息
+		addFieldText(LabelType.PRECONDITION, getAllLabelText(caseName, LabelType.PRECONDITION));
+		//存储关键词信息
+		addFieldText(LabelType.KEY, getAllLabelText(caseName, LabelType.KEY));
+		//存储优先级信息
+		addFieldText(LabelType.RANK, getLabelText(caseName, LabelType.RANK, isMust ? "1" : "2"));
+		
+		return this;
+	}
+	
+	/**
+	 * 用于生成带数字限制的文本框测试用例，当只有单边限制时，则可通过类名，调用{@link #MAX_NUMBER}与{@link #MIN_NUMBER}来获取整形最大值与最小值，例如：<br>
+	 * 1.输入限制数字最小输入为2时：addNumberRuleTextboxCase(..., 2, InformationCase.MAX_NUMBER, ...)<br>
+	 * 2.输入限制数字最大输入10时：addLengthRuleTextboxCase(..., InformationCase.MIN_NUMBER, 10, ...)<br>
+	 * 3.输入限制数字为2~10之间时：addLengthRuleTextboxCase(..., 2, 10, ...)<br>
+	 * 注意：若两个参数传入一样或最小参数为{@link #MIN_NUMBER}， 最大参数为{@link #MAX_NUMBER}，则抛出异常
+	 * @param name 文本框名称（控件名称）
+	 * @param isMust 是否必填
+	 * @param isRepeat 是否可以与存在的内容重复
+	 * @param isClear 是否有按钮可以清空文本框
+	 * @param isDecimals 数字是否是小数
+	 * @param minNum 最小数字限制
+	 * @param maxNum 最大数字限制
+	 * @param inputRuleTypes 输入限制（{@link InputRuleType}枚举类）
+	 * @return 类本身
+	 * @throws CaseContentException 当限制参数传入有误时抛出
+	 */
+	public Case addNumberRuleTextboxCase(String name, boolean isMust, boolean isRepeat, boolean isClear, boolean isDecimals, int minNum, int maxNum) {
+		//清空字段的内容
+		clearFieldText();
+		// 存储case标签的name属性内容
+		String caseName = "addTextboxCase";
+		//添加控件名称
+		wordMap.put(WordType.CONTROL_NAME.getName(), name);
+		
+		//存储标题信息
+		addFieldText(LabelType.TITLE, getLabelText(caseName, LabelType.TITLE, "1"));
+		
+		//----------------------------------------------------------------------
+		//存储步骤与预期信息
+		//添加通用用例
+		textboxCommonCase(caseName, isMust, isRepeat, isClear, InputRuleType.NUM);
+		
+		//添加输入数字限制相关的测试用例
+		//判断最大最小值是否一致，一致则抛出异常
+		if (minNum == maxNum) {
+			throw new CaseContentException("大小限制不能相同");
+		}
+		//判断最大与最小值是否需要调换，存储正确最大最小值
+		int min = minNum < maxNum ?  minNum : maxNum;
+		int max = minNum < maxNum ?  maxNum : minNum;
+		
+		//存储数字限制
+		wordMap.put(WordType.INPUT_MAX_LENGTH.getName(), String.valueOf(min));
+		wordMap.put(WordType.INPUT_MAX_NUMBER.getName(), String.valueOf(max));
+		
+		//根据两个值的结果添加相应的用例
+		if (min == MIN_NUMBER && max == MAX_NUMBER) {
+			//若两个值均为极值，则抛出异常
+			throw new CaseContentException("大小限制不能同时为极值");
+		} else if (min == MIN_NUMBER && max != MAX_NUMBER) {
+			//大于最大限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "10"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
+			//等于最大限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "11"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
+			//小于最小负数限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "8"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "5"));
+		} else if (min != MIN_NUMBER && max == MAX_NUMBER) {
+			//小于最小限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "8"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
+			//等于最小限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "11"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
+			//大于最大正数限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "10"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "5"));
+		} else {
+			//小于最小限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "8"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
+			//等于最小限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "11"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
+			//大于最大限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "10"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
+			//等于最大限制
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "11"));
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
+		}
+		
+		//----------------------------------------------------------------------
+		
+		//存储前置条件信息
+		addFieldText(LabelType.PRECONDITION, getAllLabelText(caseName, LabelType.PRECONDITION));
+		//存储关键词信息
+		addFieldText(LabelType.KEY, getAllLabelText(caseName, LabelType.KEY));
+		//存储优先级信息
+		addFieldText(LabelType.RANK, getLabelText(caseName, LabelType.RANK, isMust ? "1" : "2"));
+		
+		return this;
+	}
+	
+	/**
+	 * 用于生成文本框类型共同的测试用例步骤与预期（即普通文本框测试用例的步骤与预期）
+	 * @param name 文本框名称（控件名称）
+	 * @param isMust 是否必填
+	 * @param isRepeat 是否可以与存在的内容重复
+	 * @param isClear 是否有按钮可以清空文本框
+	 */
+	private void textboxCommonCase(String name, boolean isMust, boolean isRepeat, boolean isClear, InputRuleType... inputRuleTypes) {
+		// 存储case标签的name属性内容
+		String caseName = "addTextboxCase";
+		//转换输入限制为集合类型
+		List<InputRuleType> inputRules = Arrays.asList(inputRuleTypes);
+		
 		//不填写或只输入空格
 		addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "1"));
 		//根据是否必填来判断填入成功或失败预期
@@ -242,65 +404,13 @@ public class InformationCase extends Case {
 			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
 		}
 		
-		//填写存在的内容
-		addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "12"));
-		//根据是否必填来判断填入成功或失败预期
-		addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, isRepeat ? "2" : "1"));
-		
-		//添加输入长度限制相关的测试用例
-		//判断传入的限制参数是否有小于0的参数，参数小于0则直接转换为0
-		minLen = minLen < 0 ? 0 : minLen;
-		maxLen = maxLen < 0 ? 0 : maxLen;
-		//判断长度是否都为0，若都为0，则抛出异常
-		if (minLen == 0 && maxLen == 0) {
-			throw new CaseContentException("长度限制不能全为0");
-		} else if (minLen != 0 && maxLen != 0) {
-			//若都不为0，则将相应的内容进行存储，且判断最大长度与最小长度是否传反，若传反，则调换顺序
-			wordMap.put(WordType.INPUT_MIN_LENGTH.getName(), String.valueOf(minLen < maxLen ? minLen : maxLen));
-			wordMap.put(WordType.INPUT_MAX_LENGTH.getName(), String.valueOf(minLen < maxLen ? maxLen : minLen));
-			
-			//小于最小限制
-			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "4"));
-			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
-			//等于最小限制
-			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "5"));
-			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
-			//大于最大限制
-			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "6"));
-			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
-			//等于最大限制
-			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "7"));
-			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
-		} else if (minLen == 0 && maxLen != 0 || minLen == maxLen) {
-			//存储最大输入限制
-			wordMap.put(WordType.INPUT_MAX_LENGTH.getName(), String.valueOf(maxLen));
-			//大于最大限制
-			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "6"));
-			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
-			//等于最大限制
-			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "7"));
-			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
-		} else {
-			//存储最小输入限制
-			wordMap.put(WordType.INPUT_MIN_LENGTH.getName(), String.valueOf(minLen));
-			
-			//小于最小限制
-			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "4"));
-			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "2"));
-			//等于最小限制
-			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "5"));
-			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, "1"));
+		//清空输入的内容
+		if (isClear) {
+			//不填写或只输入空格
+			addFieldText(LabelType.STEP, getLabelText(caseName, LabelType.STEP, "13"));
+			//根据是否必填来判断填入成功或失败预期
+			addFieldText(LabelType.EXCEPT, getLabelText(caseName, LabelType.EXCEPT, isMust ? "4" : "3"));
 		}
-		//----------------------------------------------------------------------
-		
-		//存储前置条件信息
-		addFieldText(LabelType.PRECONDITION, getAllLabelText(caseName, LabelType.PRECONDITION));
-		//存储关键词信息
-		addFieldText(LabelType.KEY, getAllLabelText(caseName, LabelType.KEY));
-		//存储优先级信息
-		addFieldText(LabelType.RANK, getLabelText(caseName, LabelType.RANK, isMust ? "1" : "2"));
-		
-		return this;
 	}
 	
 	/**
@@ -308,7 +418,7 @@ public class InformationCase extends Case {
 	 * <p><b>用途：</b>枚举在预设测试用例中需要被替换的词语</p>
 	 * <p><b>编码时间：</b>2020年3月15日下午5:50:28</p>
 	 * <p><b>修改时间：</b>2020年3月15日下午5:50:28</p>
-	 * @author 
+	 * @author 彭宇琦
 	 * @version Ver1.0
 	 * @since JDK 12
 	 *
--- src/test/java/pres/auxiliary/work/testcase/InformationCaseTest.java
+++ src/test/java/pres/auxiliary/work/testcase/InformationCaseTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import pres.auxiliary.work.n.tcase.CaseContentException;
 import pres.auxiliary.work.n.tcase.InformationCase;
 import pres.auxiliary.work.n.tcase.InputRuleType;
 
@@ -16,10 +17,10 @@ InformationCase ic = new InformationCase(new File("ConfigurationFiles/CaseConfig
 	public void start() {
 		ic.setReplaceWord(InformationCase.BUTTON_NAME, "保存");
 		ic.setReplaceWord(InformationCase.ADD_INFORMATION, "用户列表");
-		ic.setReplaceWord(InformationCase.FAIL_EXCEPT_TEXT_START, "测试失败预期前文");
-		ic.setReplaceWord(InformationCase.FAIL_EXCEPT_TEXT_END, "测试失败预期后文");
-		ic.setReplaceWord(InformationCase.SUCCESS_EXCEPT_TEXT_START, "测试成功预期前文");
-		ic.setReplaceWord(InformationCase.SUCCESS_EXCEPT_TEXT_END, "测试成功预期后文");
+		ic.setReplaceWord(InformationCase.FAIL_EXCEPT_TEXT_START, "测试*失败*预期前文");
+		ic.setReplaceWord(InformationCase.FAIL_EXCEPT_TEXT_END, "测试*失败*预期后文");
+		ic.setReplaceWord(InformationCase.SUCCESS_EXCEPT_TEXT_START, "测试*成功*预期前文");
+		ic.setReplaceWord(InformationCase.SUCCESS_EXCEPT_TEXT_END, "测试*成功*预期后文");
 	}
 	
 	@AfterMethod
@@ -48,18 +49,64 @@ InformationCase ic = new InformationCase(new File("ConfigurationFiles/CaseConfig
 	}
 	
 	/**
-	 * 测试{@link InformationCase#addBasicTextboxCase(String, boolean, boolean, pres.auxiliary.work.n.tcase.InputRuleType...)}方法
+	 * 测试{@link InformationCase#addBasicTextboxCase(String, boolean, boolean, boolean, pres.auxiliary.work.n.tcase.InputRuleType...)}方法
 	 */
 	@Test
 	public void addBasicTextboxCaseTest_NotRule() {
-		ic.addBasicTextboxCase("测试控件1", true, false);
+		ic.addBasicTextboxCase("测试控件1", true, true, true);
 	}
 	
 	/**
-	 * 测试{@link InformationCase#addBasicTextboxCase(String, boolean, boolean, pres.auxiliary.work.n.tcase.InputRuleType...)}方法
+	 * 测试{@link InformationCase#addBasicTextboxCase(String, boolean, boolean, boolean, pres.auxiliary.work.n.tcase.InputRuleType...)}方法
 	 */
 	@Test
 	public void addBasicTextboxCaseTest_HaveRule() {
-		ic.addBasicTextboxCase("测试控件2", false, true, InputRuleType.EN, InputRuleType.NUM);
+		ic.addBasicTextboxCase("测试控件2", false, false, false, InputRuleType.EN, InputRuleType.NUM);
+	}
+	
+	/**
+	 * 测试{@link InformationCase#addLengthRuleTextboxCase(String, boolean, boolean, boolean, int, int, InputRuleType...)}方法
+	 */
+	@Test
+	public void aaddLengthRuleTextboxCaseTest_1() {
+		ic.addLengthRuleTextboxCase("测试控件3", true, true, true, 2, 5);
+	}
+	
+	/**
+	 * 测试{@link InformationCase#addLengthRuleTextboxCase(String, boolean, boolean, boolean, int, int, InputRuleType...)}方法
+	 */
+	@Test
+	public void aaddLengthRuleTextboxCaseTest_2() {
+		ic.addLengthRuleTextboxCase("测试控件4", false, false, true, 5, 5, InputRuleType.EN, InputRuleType.NUM);
+	}
+	/**
+	 * 测试{@link InformationCase#addLengthRuleTextboxCase(String, boolean, boolean, boolean, int, int, InputRuleType...)}方法
+	 */
+	@Test
+	public void aaddLengthRuleTextboxCaseTest_3() {
+		ic.addLengthRuleTextboxCase("测试控件5", false, false, false, 0, 5);
+	}
+	/**
+	 * 测试{@link InformationCase#addLengthRuleTextboxCase(String, boolean, boolean, boolean, int, int, InputRuleType...)}方法
+	 */
+	@Test
+	public void aaddLengthRuleTextboxCaseTest_4() {
+		ic.addLengthRuleTextboxCase("测试控件6", false, false, false, 2, -5);
+	}
+	
+	/**
+	 * 测试{@link InformationCase#addLengthRuleTextboxCase(String, boolean, boolean, boolean, int, int, InputRuleType...)}方法
+	 */
+	@Test
+	public void aaddLengthRuleTextboxCaseTest_5() {
+		ic.addLengthRuleTextboxCase("测试控件7", false, false, false, 5, 2);
+	}
+	
+	/**
+	 * 测试{@link InformationCase#addLengthRuleTextboxCase(String, boolean, boolean, boolean, int, int, InputRuleType...)}方法
+	 */
+	@Test(expectedExceptions = {CaseContentException.class})
+	public void aaddLengthRuleTextboxCaseTest_6() {
+		ic.addLengthRuleTextboxCase("测试控件8", false, false, false, -54, 0);
 	}
 }
