c9179f4caab982b748bc1913d060448cc046f59b
彭宇琦 <pyqone@outlook.com>
2020-06-21 18:32:06 +0800

    添加接口传参的部分方法

--- src/main/java/pres/auxiliary/tool/http/EasyHttp.java
+++ src/main/java/pres/auxiliary/tool/http/EasyHttp.java
@@ -1,5 +1,10 @@
 package pres.auxiliary.tool.http;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
+import com.alibaba.fastjson.JSONObject;
+
 /**
  * <p><b>文件名：</b>EasyHttp.java</p>
  * <p><b>用途：</b>
@@ -11,7 +16,7 @@ package pres.auxiliary.tool.http;
  * @version Ver1.0
  *
  */
-public class EasyHttp {
+public class EasyHttp implements Cloneable {
 	/**
 	 * 存储指定的请求方式
 	 */
@@ -39,6 +44,20 @@ public class EasyHttp {
 	 */
 	private String address = "";
 	
+	/**
+	 * 存储接口请求的参数
+	 */
+	private HashMap<String, String> parameterMap = new HashMap<>(16);
+	
+	/**
+	 * 用于存储请求体
+	 */
+	private String body = "";
+	
+	/**
+	 * 用于存储请求头
+	 */
+	private HashMap<String, String> headMap = new HashMap<>();
 	
 	/**
 	 * 用于设置请求方式，传入请求方式枚举类{@link RequestType}
@@ -57,14 +76,14 @@ public class EasyHttp {
 	 */
 	public EasyHttp url(String url) {
 		String inter = "";
-		String pram = "";
+		String param = "";
 		
 		//替换中文冒号为英文冒号
 		inter.replaceAll("：", ":");
 		//判断传入的url是否存在参数
 		if (url.indexOf("?") > -1) {
 			inter = url.substring(0, url.indexOf("?"));
-			pram = url.substring(url.indexOf("?") + 1);
+			param = url.substring(url.indexOf("?") + 1);
 		} else {
 			inter = url;
 		}
@@ -94,6 +113,11 @@ public class EasyHttp {
 			//若其中未包含冒号，则表示端口为80
 			port("80");
 		}
+		
+		//裁剪参数
+		Arrays.stream(param.split("&")).forEach(parameterText -> {
+			putParameter(parameterText);
+		});
 		
 		return this;
 	}
@@ -139,11 +163,79 @@ public class EasyHttp {
 	}
 	
 	/**
+	 * 用于设置请求参数，若参数名存在时，则覆盖上一次设置的值
+	 * @param key 参数名
+	 * @param value 参数值
+	 * @return 类本身
+	 */
+	public EasyHttp putParameter(String key, String value) {
+		parameterMap.put(key, value);
+		return this;
+	}
+	
+	/**
+	 * 用于设置“key=value”格式的参数，若参数名存在时，则覆盖上一次设置的值
+	 * @param parameterText 参数文本
+	 * @return 类本身
+	 * @see #putParameter(String, String)
+	 */
+	public EasyHttp putParameter(String parameterText) {
+		String[] parameter = parameterText.split("=");
+		//去空格后传入putParameter方法中
+		return putParameter(parameter[0].trim(), parameter[1].trim());
+	}
+	
+	/**
+	 * 用于设置接口的请求体
+	 * @param body 请求体
+	 * @return 类本身
+	 */
+	public EasyHttp body(String body) {
+		this.body = body;
+		return this;
+	}
+	
+	/**
+	 * 用于设置请求头，若请求头名存在时，则覆盖上一次设置的值
+	 * @param key 请求头名
+	 * @param value 请求头值
+	 * @return 类本身
+	 */
+	public EasyHttp putHead(String key, String value) {
+		headMap.put(key, value);
+		return this;
+	}
+	
+	/**
 	 * 返回设置的url
 	 * @return url
 	 */
 	public String getUrlString() {
+		//删除已存储的url值
 		url.delete(0, url.length());
-		return url.append(agreement).append(host).append(":" + port).append(address).toString();
+		//拼接接口请求地址
+		url.append(agreement).append(host).append(":" + port).append(address).toString();
+		
+		//拼接接口请求参数，若无请求参数，则不拼接参数
+		if (!parameterMap.isEmpty()) {
+			url.append("?");
+			
+			//遍历parameterMap，获取参数值
+			parameterMap.forEach((key, value) -> {
+				url.append(key).append("=").append(value).append("&");
+			});
+			
+			//删除多余的&符号
+			url.delete(url.lastIndexOf("&"), url.length());
+		}
+		
+		//判断请求方式是否为get请求，若为get请求时，若设置了body，则将body拼接至末尾
+		if (requestType == RequestType.GET) {
+			//根据parameterMap是否为空判断，应拼接何种连接符，若parameterMap存在参数，则拼接"&"符号
+			url.append(parameterMap.isEmpty() ? "?" : "&");
+		}
+		
+		//返回参数，并将空格转换为20%
+		return url.toString().replaceAll(" ", "20%");
 	}
 }
