d59ab637a2ff711cc3eaa5b10fe8d49351a14ac6
彭宇琦 <465645774@qq.com>
2020-12-31 10:11:49 +0800

    添加持续执行方法与循环执行方法

--- src/main/java/pres/auxiliary/work/selenium/event/extend/GroupEvent.java
+++ src/main/java/pres/auxiliary/work/selenium/event/extend/GroupEvent.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 /**
  * <p>
@@ -30,6 +31,20 @@ public class GroupEvent {
 	 * 用于存储事件的组合
 	 */
 	HashMap<String, EvenetAction> eventMap = new HashMap<>();
+
+	/**
+	 * 用于控制在连续执行事件时是否因为执行失败而中断执行
+	 */
+	boolean isExceptionBreak = true;
+
+	/**
+	 * 设置在连续执行事件时，是否由于某一方法执行失败而终止运行
+	 * 
+	 * @param isExceptionBreak 失败是否继续执行
+	 */
+	public void setExceptionBreak(boolean isExceptionBreak) {
+		this.isExceptionBreak = isExceptionBreak;
+	}
 
 	/**
 	 * 用于添加无参数的事件组合
@@ -145,39 +160,70 @@ public class GroupEvent {
 	}
 
 	/**
-	 * 用于按照顺序执行事件，根据参数，判断事件执行失败时是否中断执行。
+	 * 用于按照顺序执行事件。
 	 * <p>
 	 * <b>注意：</b>
 	 * <ol>
-	 * <li>若设置失败不中断执行时，则其相应的执行结果存储为{@link Optional#empty()}</li>
+	 * <li>若在{@link #setExceptionBreak(boolean)}方法中设置false时，则当事件运行失败后，其相应的执行结果存储为{@link Optional#empty()}，并继续运行下一个事件组</li>
 	 * <li>设置的是否中断执行，只控制执行方法中产生的异常，若因方法名称不存在而抛出异常时，则同样会中断运行</li>
 	 * </ol>
 	 * </p>
 	 * 
-	 * @param isExceptionBreak 方法执行失败时是否终止执行
-	 * @param eventNames       执行方法名称组
+	 * @param eventNames 执行方法名称组
 	 * @return 方法执行结果集合
 	 * @throws ParamException 原事件组名称未指定或事件组名称不存在时抛出的异常
 	 */
-	public List<Optional<Object>> continuousAction(boolean isExceptionBreak, String... eventNames) {
+	public List<Optional<Object>> orderlyAction(String... eventNames) {
 		List<Optional<Object>> resultList = new ArrayList<>();
 		Arrays.stream(Optional.ofNullable(eventNames).filter(names -> names.length != 0)
-				.orElseThrow(() -> new ParamException("未指定事件名称组")))
-			.map(name -> {
-				//执行名称判断方法，若方法名称不存在，则抛出异常
-				judgeParam(name);
-				//执行方法，若方法抛出异常，则根据是否中断运行，来给出相应的结果
-				try {
-					return action(name);
-				} catch (Exception e) {
-					if (isExceptionBreak) {
-						throw e;
-					} else {
-						return Optional.empty();
-					}
-				}
-			}).forEach(resultList::add);
+				.orElseThrow(() -> new ParamException("未指定事件名称组"))).map(this::getContinueResult)
+				.forEach(resultList::add);
 
+		return resultList;
+	}
+
+	/**
+	 * 用于按照顺序执行事件，并将每条事件的结果进行判断，若判断不通过，则终止运行。
+	 * 
+	 * @param predicate  继续执行条件，其参数为上一个事件组执行的结果
+	 * @param eventNames 执行方法名称组
+	 * @return 方法执行结果集合
+	 * @throws ParamException 原事件组名称未指定或事件组名称不存在时抛出的异常
+	 * @see #orderlyAction(String...)
+	 */
+	public List<Optional<Object>> orderlyAction(Predicate<Optional<Object>> predicate, String... eventNames) {
+		List<Optional<Object>> resultList = new ArrayList<>();
+		for (String name : eventNames) {
+			// 获取相应的执行结果
+			Optional<Object> result = getContinueResult(name);
+			// 若结果不符合筛选条件，则结束循环
+			if (!predicate.test(result)) {
+				break;
+			}
+
+			resultList.add(result);
+		}
+
+		return resultList;
+	}
+	
+	/**
+	 * 用于循环执行一组事件组，并将每个结果进行判断，若判断不通过，则终止循环，并返回最后一次事件组的执行结果
+	 * <p>
+	 * <b>注意：</b>无论条件如何设置，其事件组都会执行一次
+	 * </p>
+	 * @param predicate 继续执行条件，其参数为上一次事件组执行的一组结果
+	 * @param eventNames 执行方法名称组
+	 * @return 最后一次循环方法执行结果集合
+	 * @throws ParamException 原事件组名称未指定或事件组名称不存在时抛出的异常
+	 * @see #orderlyAction(String...)
+	 */
+	public List<Optional<Object>> whileAction(Predicate<List<Optional<Object>>> predicate, String... eventNames) {
+		List<Optional<Object>> resultList = new ArrayList<>();
+		do {
+			resultList = orderlyAction(eventNames);
+		} while (predicate.test(resultList));
+		
 		return resultList;
 	}
 
@@ -191,6 +237,27 @@ public class GroupEvent {
 	private String judgeParam(String name) {
 		return Optional.ofNullable(name).filter(eventMap::containsKey)
 				.orElseThrow(() -> new ParamException("未指定执行名称或名称不存在：" + name));
+	}
+
+	/**
+	 * 集成在持续运行中相应事件组的执行结果
+	 * 
+	 * @param name 事件组名称
+	 * @return 相应事件组的执行结果
+	 */
+	private Optional<Object> getContinueResult(String name) {
+		// 执行名称判断方法，若方法名称不存在，则抛出异常
+		judgeParam(name);
+		// 执行方法，若方法抛出异常，则根据是否中断运行，来给出相应的结果
+		try {
+			return action(name);
+		} catch (Exception e) {
+			if (isExceptionBreak) {
+				throw e;
+			} else {
+				return Optional.empty();
+			}
+		}
 	}
 
 	/**
