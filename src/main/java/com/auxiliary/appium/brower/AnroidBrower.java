a2c49b81dcac7735a7b0ff97f62806d2e775a434
彭宇琦 <465645774@qq.com>
2021-04-08 14:45:33 +0800

    调整代码格式，修改不合规范的内容

--- src/main/java/com/auxiliary/appium/brower/AnroidBrower.java
+++ src/main/java/com/auxiliary/appium/brower/AnroidBrower.java
@@ -9,21 +9,29 @@ import org.openqa.selenium.WebElement;
 import io.appium.java_client.android.AndroidDriver;
 
 /**
- * <p><b>文件名：</b>AnroidBrower.java</p>
- * <p><b>用途：</b>
- * 定义启动安卓手机app自动化相关的方法
+ * <p>
+ * <b>文件名：</b>AnroidBrower.java
  * </p>
- * <p><b>编码时间：</b>2021年4月7日下午6:52:38</p>
- * <p><b>修改时间：</b>2021年4月7日下午6:52:38</p>
+ * <p>
+ * <b>用途：</b> 定义启动安卓手机app自动化相关的方法
+ * </p>
+ * <p>
+ * <b>编码时间：</b>2021年4月7日下午6:52:38
+ * </p>
+ * <p>
+ * <b>修改时间：</b>2021年4月7日下午6:52:38
+ * </p>
+ * 
  * @author 彭宇琦
  * @version Ver1.0
  * @since JDK 1.8
  */
-public class AnroidBrower extends CellphoneBrower {
+public class AnroidBrower extends AbstractCellphoneBrower {
 	/**
 	 * 构造对象，并初始化设备名称以及待测试app包信息，默认使用“uiautomator2”执行器，并清除app信息后启动
+	 * 
 	 * @param deviceName 设备名称
-	 * @param linkUrl appium的连接地址
+	 * @param linkUrl    appium的连接地址
 	 * @param appPackage app包信息
 	 */
 	public AnroidBrower(String deviceName, URL linkUrl, AppPackage appPackage) {
@@ -32,9 +40,10 @@ public class AnroidBrower extends CellphoneBrower {
 
 	/**
 	 * 构造对象，并初始化设备名称以及待测试app包信息，默认使用“uiautomator2”执行器，并清除app信息后启动
-	 * @param deviceName 设备名称
-	 * @param linkUrl appium的连接地址
-	 * @param packageName app包名称
+	 * 
+	 * @param deviceName   设备名称
+	 * @param linkUrl      appium的连接地址
+	 * @param packageName  app包名称
 	 * @param activityName app启动类名称
 	 */
 	public AnroidBrower(String deviceName, URL linkUrl, String packageName, String activityName) {
@@ -48,6 +57,7 @@ public class AnroidBrower extends CellphoneBrower {
 
 	@Override
 	protected Map<String, Object> getCapabilities() {
-		return Optional.ofNullable(((AndroidDriver<?>)driver)).map(d -> d.getCapabilities().asMap()).orElse(driverInfo.asMap());
+		return Optional.ofNullable(((AndroidDriver<?>) driver)).map(d -> d.getCapabilities().asMap())
+				.orElse(driverInfo.asMap());
 	}
 }
