e60156b4ca72cae8ba4c2c477b6cf2e9d59134d9
彭宇琦 <465645774@qq.com>
2021-07-12 15:16:17 +0800

    修改接口方法，在模板中添加相关的部分脚本数据

--- src/main/java/com/auxiliary/testcase/templet/CreateAutoSriptCase.java
+++ src/main/java/com/auxiliary/testcase/templet/CreateAutoSriptCase.java
@@ -2,27 +2,44 @@ package com.auxiliary.testcase.templet;
 
 import java.io.File;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.auxiliary.testcase.file.IncorrectFileException;
+
 /**
  * <p><b>文件名：</b>CreateAutoSriptCase.java</p>
  * <p><b>用途：</b>
+ * 指定能生成自动化测试脚本的测试用例模板所必须的方法
  * </p>
  * <p><b>编码时间：</b>2021年7月9日上午8:20:53</p>
  * <p><b>修改时间：</b>2021年7月9日上午8:20:53</p>
- * @author 
+ * @author 彭宇琦
  * @version Ver1.0
  * @since JDK 1.8
  */
 public abstract class CreateAutoSriptCase extends Case implements GetAutoScript {
-
+	/**
+	 * 用于存储脚本相关的Json
+	 */
+	protected JSONObject scriptJson = new JSONObject();
+	
+	/**
+	 * 根据用例xml文件来构造用例模板类
+	 * 
+	 * @param configXmlFile xml配置文件
+	 * @throws IncorrectFileException 文件格式或路径不正确时抛出的异常
+	 */
 	public CreateAutoSriptCase(File configXmlFile) {
 		super(configXmlFile);
-		// TODO Auto-generated constructor stub
+		
+		// 初始化json
+		scriptJson.put(KEY_ELEMENT, new JSONArray());
+		scriptJson.put(KEY_CASE, new JSONArray());
 	}
 
 	@Override
 	public String getAutoScriptJson() {
-		// TODO Auto-generated method stub
-		return null;
+		return scriptJson.toJSONString();
 	}
 
 }
