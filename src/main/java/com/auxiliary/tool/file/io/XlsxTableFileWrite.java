aa61c1a8d81641a74440cdc38f08e04d622506bb
彭宇琦 <465615774@qq.com>
2021-09-23 10:21:07 +0800

    修改表格类写入方法

--- src/main/java/com/auxiliary/tool/file/io/XlsxTableFileWrite.java
+++ src/main/java/com/auxiliary/tool/file/io/XlsxTableFileWrite.java
@@ -1,10 +1,18 @@
 package com.auxiliary.tool.file.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.StringJoiner;
 
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.auxiliary.tool.data.TableData;
+import com.auxiliary.tool.file.WriteFileException;
+import com.auxiliary.tool.file.excel.ExcelFileTemplet;
 
 /**
  * <p><b>文件名：</b>XlsxTableFileWrite.java</p>
@@ -18,17 +26,41 @@ import com.auxiliary.tool.data.TableData;
  * @since JDK 1.8
  */
 public class XlsxTableFileWrite extends AbstractTableFileWrite {
-	XSSFWorkbook write;
+	/**
+	 * 用于存储excel对象
+	 */
+	private XSSFWorkbook excel;
+	/**
+	 * 用于存储当前指向的sheet页
+	 */
+	private XSSFSheet sheet;
 	
+	/**
+	 * 根据文件类对象，打开相应的文件
+	 * 
+	 * @param readFile 文件对象
+	 * @throws FileException 当文件打开出错时抛出的异常
+	 */
 	public XlsxTableFileWrite(File writeFile) {
-		// TODO Auto-generated constructor stub
+		if (writeFile.exists()) {
+			try (FileInputStream fip = new FileInputStream(writeFile)) {
+				// 通过输入流，使XSSFWorkbook对象指向模版文件
+				excel = new XSSFWorkbook(fip);
+			} catch (IOException e) {
+				throw new FileException("文件读取异常", writeFile);
+			}
+		} else {
+			// 创建文件存储的路径文件夹
+			writeFile.getParentFile().mkdirs();
+			excel = new XSSFWorkbook();
+		}
 	}
 	
 	/**
 	 * @return the read
 	 */
 	public XSSFWorkbook getWriteClass() {
-		return write;
+		return null;
 	}
 
 	@Override
