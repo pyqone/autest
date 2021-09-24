fc990e44d92c0c851adec092e54dbd43465dcf06
彭宇琦 <465615774@qq.com>
2021-09-24 11:05:17 +0800

    完成快速写入列表数据方法

--- src/main/java/com/auxiliary/tool/file/excel/WriteBasicExcelTempletFile.java
+++ src/main/java/com/auxiliary/tool/file/excel/WriteBasicExcelTempletFile.java
@@ -50,20 +50,33 @@ public class WriteBasicExcelTempletFile extends WriteExcelTempletFile<WriteBasic
 	public WriteBasicExcelTempletFile(String templetName, FileTemplet templet) {
 		super(templetName, templet);
 	}
-	
+
 	/**
+	 * 使用默认的excel模板，快速将列表数据类对象中的数据，写入到指定的文件中
 	 * 
-	 * @param saveFile
-	 * @param data
-	 * @return
+	 * @param saveFile 存储数据的文件对象
+	 * @param data     表格数据类对象
+	 * @return saveFile指向的文件类对象
 	 */
 	public static File writeTabelData(File saveFile, TableData<String> data) {
-		// 设置模板
-		ExcelFileTemplet templet = new ExcelFileTemplet(saveFile);
+		return writeTabelData(new ExcelFileTemplet(saveFile), data);
+	}
+
+	/**
+	 * 用于根据指定的模板类对象，将表格数据类对象中的数据，写入到指定的文件中。
+	 * <p>
+	 * <b>注意：</b>若模板类对象中不存在表格数据类对象中的字段（不是名称）时，则程序会自动创建该字段
+	 * </p>
+	 * 
+	 * @param templet 模板类对象
+	 * @param data    数据类对象
+	 * @return 模板对象中存储的文件所指向的对象
+	 */
+	public static File writeTabelData(FileTemplet templet, TableData<String> data) {
 		// 读取表格数据中的字段并写入到模板的字段中，作为模板中的标题
 		List<String> titleList = data.getColumnName();
 		titleList.forEach(templet::addField);
-		
+
 		// 构造模板
 		WriteBasicExcelTempletFile excel = new WriteBasicExcelTempletFile("默认模板", templet);
 		// 获取行下标
@@ -75,8 +88,8 @@ public class WriteBasicExcelTempletFile extends WriteExcelTempletFile<WriteBasic
 			}
 			excel.end();
 		}
-		
+
 		excel.write();
-		return saveFile;
+		return new File(templet.getTempletAttribute(FileTemplet.KEY_SAVE).toString());
 	}
 }
