7eb63fb274e27f5b4968edb708fda233668cca94
彭宇琦 <465615774@qq.com>
2021-10-18 10:34:37 +0800

    完成断言事件类改造

--- src/main/java/com/auxiliary/selenium/event/AssertEvent.java
+++ src/main/java/com/auxiliary/selenium/event/AssertEvent.java
@@ -21,12 +21,12 @@ import com.auxiliary.tool.asserts.AssertStringUtil;
  * <b>编码时间：</b>2020年10月20日上午7:48:05
  * </p>
  * <p>
- * <b>修改时间：</b>2020年10月20日上午7:48:05
+ * <b>修改时间：</b>2021年10月18日 上午10:25:01
  * </p>
  * 
  * @author 彭宇琦
  * @version Ver1.0
- *
+ * @since JDK 1.8
  */
 public class AssertEvent extends AbstractEvent {
 	/**
@@ -60,107 +60,109 @@ public class AssertEvent extends AbstractEvent {
 
 	/**
 	 * <p>
-	 * 	断言元素的文本内容中包含指定的关键词。可设置元素的文本内容是否需要判断传入的所有关键词，即
-	 * 	<ul>
-	 * 		<li>当需要完全判断时，则当且仅当文本包含所有关键词时，断言方法返回true</li>
-	 * 		<li>当不需要完全判断时，则当前仅当文本不包含所有关键词时，断言方法返回false</li>
-	 * 	</ul>
+	 * 断言元素的文本内容中包含指定的关键词。可设置元素的文本内容是否需要判断传入的所有关键词，即
+	 * <ul>
+	 * <li>当需要完全判断时，则当且仅当文本包含所有关键词时，断言方法返回true</li>
+	 * <li>当不需要完全判断时，则当前仅当文本不包含所有关键词时，断言方法返回false</li>
+	 * </ul>
 	 * </p>
 	 * <p>
-	 * 	例如,假设元素中存在文本“你好，世界！”，则
-	 * 	<ul>
-	 * 		<li>需要判断的关键词为["你好", "Java"]时：
-	 * 			<ol>
-	 * 				<li>需要判断所有文本，此时断言方法返回false</li>
-	 * 				<li>不需要判断所有文本，此时断言方法返回true</li>
-	 * 			</ol>
-	 * 		</li>
-	 * 		<li>需要判断的关键词为["你好", "世界"]时：
-	 * 			<ol>
-	 * 				<li>需要判断所有文本，此时断言方法返回true</li>
-	 * 				<li>不需要判断所有文本，此时断言方法返回true</li>
-	 * 			</ol>
-	 * 		</li>
-	 * 		<li>需要判断的关键词为["Hello", "Java"]时：
-	 * 			<ol>
-	 * 				<li>需要判断所有文本，此时断言方法返回false</li>
-	 * 				<li>不需要判断所有文本，此时断言方法返回false</li>
-	 * 			</ol>
-	 * 		</li>
-	 * 	</ul>
+	 * 例如,假设元素中存在文本“你好，世界！”，则
+	 * <ul>
+	 * <li>需要判断的关键词为["你好", "Java"]时：
+	 * <ol>
+	 * <li>需要判断所有文本，此时断言方法返回false</li>
+	 * <li>不需要判断所有文本，此时断言方法返回true</li>
+	 * </ol>
+	 * </li>
+	 * <li>需要判断的关键词为["你好", "世界"]时：
+	 * <ol>
+	 * <li>需要判断所有文本，此时断言方法返回true</li>
+	 * <li>不需要判断所有文本，此时断言方法返回true</li>
+	 * </ol>
+	 * </li>
+	 * <li>需要判断的关键词为["Hello", "Java"]时：
+	 * <ol>
+	 * <li>需要判断所有文本，此时断言方法返回false</li>
+	 * <li>不需要判断所有文本，此时断言方法返回false</li>
+	 * </ol>
+	 * </li>
+	 * </ul>
 	 * </p>
 	 * <p>
-	 * 	<b>注意：</b>若不传入关键词，或关键词为null时，则返回true
+	 * <b>注意：</b>若不传入关键词，或关键词为null时，则返回true
 	 * </p>
 	 * 
 	 * @param element       {@link Element}对象
 	 * @param isJudgeAllKey 是否需要完全判断所有关键词
 	 * @param keys          关键词组
 	 * @return 断言结果
-	 * @throws TimeoutException 元素无法操作时抛出的异常 
-	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常 
+	 * @throws TimeoutException       元素无法操作时抛出的异常
+	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常
 	 */
 	public boolean assertTextContainKey(Element element, boolean isJudgeAllKey, String... keys) {
-		boolean result = assertTextContainKey(element, (e) -> textEvent.getText(e), 
-				isJudgeAllKey, keys);
-		
-		logText = "断言“" + element.getElementData().getName() + "”元素的文本内容包含"
-				+ (isJudgeAllKey ? "所有" : "部分") + "关键词" + arrayToString(keys);
-		resultText = String.valueOf(result);
-		
+		boolean result = assertTextContainKey(element, (e) -> textEvent.getText(e), isJudgeAllKey, keys);
+
+		// 由于调用获取文本方法，故需要删除前两条日志
+		brower.getLogRecord().removeLog(2);
+		String logText = "断言“%s”元素的文本内容包含%s关键词：%s";
+		brower.getLogRecord().recordLog(String.format(logText, element.getElementData().getName(),
+				(isJudgeAllKey ? "所有" : "部分"), arrayToString(keys)));
+
 		return result;
 	}
-	
+
 	/**
 	 * <p>
-	 * 	断言元素的文本内容中不包含指定的关键词。可设置元素的文本内容是否需要判断传入的所有关键词，即
-	 * 	<ul>
-	 * 		<li>当需要完全判断时，则当且仅当文本不包含所有关键词时，断言方法返回true</li>
-	 * 		<li>当不需要完全判断时，则当前仅当文本包含所有关键词时，断言方法返回false</li>
-	 * 	</ul>
+	 * 断言元素的文本内容中不包含指定的关键词。可设置元素的文本内容是否需要判断传入的所有关键词，即
+	 * <ul>
+	 * <li>当需要完全判断时，则当且仅当文本不包含所有关键词时，断言方法返回true</li>
+	 * <li>当不需要完全判断时，则当前仅当文本包含所有关键词时，断言方法返回false</li>
+	 * </ul>
 	 * </p>
 	 * <p>
-	 * 	例如,假设元素中存在文本“你好，世界！”，则
-	 * 	<ul>
-	 * 		<li>需要判断的关键词为["你好", "Java"]时：
-	 * 			<ol>
-	 * 				<li>需要判断所有文本，此时断言方法返回false</li>
-	 * 				<li>不需要判断所有文本，此时断言方法返回true</li>
-	 * 			</ol>
-	 * 		</li>
-	 * 		<li>需要判断的关键词为["你好", "世界"]时：
-	 * 			<ol>
-	 * 				<li>需要判断所有文本，此时断言方法返回false</li>
-	 * 				<li>不需要判断所有文本，此时断言方法返回false</li>
-	 * 			</ol>
-	 * 		</li>
-	 * 		<li>需要判断的关键词为["Hello", "Java"]时：
-	 * 			<ol>
-	 * 				<li>需要判断所有文本，此时断言方法返回true</li>
-	 * 				<li>不需要判断所有文本，此时断言方法返回true</li>
-	 * 			</ol>
-	 * 		</li>
-	 * 	</ul>
+	 * 例如,假设元素中存在文本“你好，世界！”，则
+	 * <ul>
+	 * <li>需要判断的关键词为["你好", "Java"]时：
+	 * <ol>
+	 * <li>需要判断所有文本，此时断言方法返回false</li>
+	 * <li>不需要判断所有文本，此时断言方法返回true</li>
+	 * </ol>
+	 * </li>
+	 * <li>需要判断的关键词为["你好", "世界"]时：
+	 * <ol>
+	 * <li>需要判断所有文本，此时断言方法返回false</li>
+	 * <li>不需要判断所有文本，此时断言方法返回false</li>
+	 * </ol>
+	 * </li>
+	 * <li>需要判断的关键词为["Hello", "Java"]时：
+	 * <ol>
+	 * <li>需要判断所有文本，此时断言方法返回true</li>
+	 * <li>不需要判断所有文本，此时断言方法返回true</li>
+	 * </ol>
+	 * </li>
+	 * </ul>
 	 * </p>
 	 * <p>
-	 * 	<b>注意：</b>若不传入关键词，或关键词为null时，则返回true
+	 * <b>注意：</b>若不传入关键词，或关键词为null时，则返回true
 	 * </p>
 	 * 
 	 * @param element       {@link Element}对象
 	 * @param isJudgeAllKey 是否需要完全判断所有关键词
 	 * @param keys          关键词组
 	 * @return 断言结果
-	 * @throws TimeoutException 元素无法操作时抛出的异常 
-	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常 
+	 * @throws TimeoutException       元素无法操作时抛出的异常
+	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常
 	 */
 	public boolean assertTextNotContainKey(Element element, boolean isJudgeAllKey, String... keys) {
-		boolean result = assertTextNotContainKey(element, (e) -> textEvent.getText(e), 
-				isJudgeAllKey, keys);
-		
-		logText = "断言“" + element.getElementData().getName() + "”元素的文本内容不包含"
-				+ (isJudgeAllKey ? "所有" : "部分") + "关键词" + arrayToString(keys);
-		resultText = String.valueOf(result);
-		
+		boolean result = assertTextNotContainKey(element, (e) -> textEvent.getText(e), isJudgeAllKey, keys);
+
+		// 由于调用获取文本方法，故需要删除前两条日志
+		brower.getLogRecord().removeLog(2);
+		String logText = "断言“%s”元素的文本内容不包含%s关键词：%s";
+		brower.getLogRecord().recordLog(String.format(logText, element.getElementData().getName(),
+				(isJudgeAllKey ? "所有" : "部分"), arrayToString(keys)));
+
 		return result;
 	}
 
@@ -173,27 +175,24 @@ public class AssertEvent extends AbstractEvent {
 	 * @param isJudgeAllKey 是否需要完全判断所有关键词
 	 * @param keys          关键词组
 	 * @return 断言结果
-	 * @throws TimeoutException 元素无法操作时抛出的异常 
-	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常 
+	 * @throws TimeoutException       元素无法操作时抛出的异常
+	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常
 	 */
 	public boolean assertAttributeContainKey(Element element, String attributeName, boolean isJudgeAllKey,
 			String... keys) {
-		//判断是否传入关键词
-//		boolean result = true;
-//		if (keys != null && keys.length != 0) {
-//			result = judgetText(textEvent.getAttributeValue(element, attributeName), isJudgeAllKey, false, keys);	
-//		}
-		boolean result = assertTextContainKey(element, (e) -> textEvent.getAttributeValue(e, attributeName), 
+		// 判断是否传入关键词
+		boolean result = assertTextContainKey(element, (e) -> textEvent.getAttributeValue(e, attributeName),
 				isJudgeAllKey, keys);
-		
-		logText = "断言“" + element.getElementData().getName() + "”元素的“" + attributeName
-				+ "”属性值包含"
-				+ (isJudgeAllKey ? "所有" : "部分") + "关键词" + arrayToString(keys);
-		resultText = String.valueOf(result);
-		
+
+		// 由于调用获取文本方法，故需要删除前两条日志
+		brower.getLogRecord().removeLog(2);
+		String logText = "断言“%s”元素的“%s”属性值包含%s关键词:%s";
+		brower.getLogRecord().recordLog(String.format(logText, element.getElementData().getName(), attributeName,
+				(isJudgeAllKey ? "所有" : "部分"), arrayToString(keys)));
+
 		return result;
 	}
-	
+
 	/**
 	 * 断言元素的文本内容中不包含指定的关键词。可设置元素的文本内容是否需要判断传入的所有关键词，
 	 * 具体参数介绍可参考{@link #assertTextNotContainKey(Element, boolean, String...)}方法
@@ -203,220 +202,253 @@ public class AssertEvent extends AbstractEvent {
 	 * @param isJudgeAllKey 是否需要完全判断所有关键词
 	 * @param keys          关键词组
 	 * @return 断言结果
-	 * @throws TimeoutException 元素无法操作时抛出的异常 
-	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常 
+	 * @throws TimeoutException       元素无法操作时抛出的异常
+	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常
 	 */
 	public boolean assertAttributeNotContainKey(Element element, String attributeName, boolean isJudgeAllKey,
 			String... keys) {
-		boolean result = assertTextNotContainKey(element, (e) -> textEvent.getAttributeValue(e, attributeName), 
+		boolean result = assertTextNotContainKey(element, (e) -> textEvent.getAttributeValue(e, attributeName),
 				isJudgeAllKey, keys);
-		
-		logText = "断言“" + element.getElementData().getName() + "”元素的“" + attributeName
-				+ "”属性值不包含"
-				+ (isJudgeAllKey ? "所有" : "部分") + "关键词" + arrayToString(keys);
-		resultText = String.valueOf(result);
-		
+
+		// 由于调用获取文本方法，故需要删除前两条日志
+		brower.getLogRecord().removeLog(2);
+		String logText = "断言“%s”元素的“%s”属性值不包含%s关键词：%s";
+		brower.getLogRecord().recordLog(String.format(logText, element.getElementData().getName(), attributeName,
+				(isJudgeAllKey ? "所有" : "部分"), arrayToString(keys)));
+
 		return result;
 	}
-	
+
 	/**
-	 * 对元素内容进行处理，并断言处理后的文本内容中包含指定的关键词。
-	 * 可设置元素的文本内容是否需要判断传入的所有关键词，具体参数介绍
+	 * 对元素内容进行处理，并断言处理后的文本内容中包含指定的关键词。 可设置元素的文本内容是否需要判断传入的所有关键词，具体参数介绍
 	 * 可参考{@link #assertTextContainKey(Element, boolean, String...)}方法
 	 * 
-	 * @param element {@link Element}对象
-	 * @param dispose 对元素内容的处理方法，传入的参数为{@link Element}对象，返回的参数为字符串
+	 * @param element       {@link Element}对象
+	 * @param dispose       对元素内容的处理方法，传入的参数为{@link Element}对象，返回的参数为字符串
 	 * @param isJudgeAllKey 是否需要完全判断所有关键词
-	 * @param keys 关键词组
+	 * @param keys          关键词组
 	 * @return 断言结果
-	 * @throws TimeoutException 元素无法操作时抛出的异常 
-	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常 
+	 * @throws TimeoutException       元素无法操作时抛出的异常
+	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常
 	 */
-	public boolean assertTextContainKey(Element element, Function<Element, String> dispose, boolean isJudgeAllKey, String... keys) {
-		//判断是否传入关键词
+	public boolean assertTextContainKey(Element element, Function<Element, String> dispose, boolean isJudgeAllKey,
+			String... keys) {
+		// 判断是否传入关键词
 		boolean result = true;
 		String text = "";
 		if (keys != null && keys.length != 0) {
 			text = dispose.apply(element);
-//			result = judgetText(text, isJudgeAllKey, false, keys);	
-			result = AssertStringUtil.assertTextContainKey(text, isJudgeAllKey, keys);	
+			result = AssertStringUtil.assertTextContainKey(text, isJudgeAllKey, keys);
 		}
-		
-		logText = "断言“" + element.getElementData().getName() + "”元素处理后的文本内容（" + text +"）包含"
-				+ (isJudgeAllKey ? "所有" : "部分") + "关键词" + arrayToString(keys);
-		resultText = String.valueOf(result);
-		
+
+		String logText = "断言“%s”元素处理后的文本内容（%s）包含%s关键词:%s";
+		brower.getLogRecord().recordLog(String.format(logText, element.getElementData().getName(), text,
+				(isJudgeAllKey ? "所有" : "部分"), arrayToString(keys)));
+
 		return result;
 	}
-	
+
 	/**
-	 * 对元素内容进行处理，并断言处理后的文本内容中不包含指定的关键词。
-	 * 可设置元素的文本内容是否需要判断传入的所有关键词，
+	 * 对元素内容进行处理，并断言处理后的文本内容中不包含指定的关键词。 可设置元素的文本内容是否需要判断传入的所有关键词，
 	 * 具体参数介绍可参考{@link #assertTextContainKey(Element, boolean, String...)}方法
 	 * 
-	 * @param element {@link Element}对象
-	 * @param dispose 对元素内容的处理方法，传入的参数为{@link Element}对象，返回的参数为字符串
+	 * @param element       {@link Element}对象
+	 * @param dispose       对元素内容的处理方法，传入的参数为{@link Element}对象，返回的参数为字符串
 	 * @param isJudgeAllKey 是否需要完全判断所有关键词
-	 * @param keys 关键词组
+	 * @param keys          关键词组
 	 * @return 断言结果
-	 * @throws TimeoutException 元素无法操作时抛出的异常 
-	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常 
+	 * @throws TimeoutException       元素无法操作时抛出的异常
+	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常
 	 */
-	public boolean assertTextNotContainKey(Element element, Function<Element, String> dispose, boolean isJudgeAllKey, String... keys) {
-		//判断是否传入关键词
+	public boolean assertTextNotContainKey(Element element, Function<Element, String> dispose, boolean isJudgeAllKey,
+			String... keys) {
+		// 判断是否传入关键词
 		boolean result = true;
 		String text = "";
 		if (keys != null && keys.length != 0) {
 			text = dispose.apply(element);
-//			result = judgetText(text, isJudgeAllKey, true, keys);	
 			result = AssertStringUtil.assertTextNotContainKey(text, isJudgeAllKey, keys);
 		}
-		
-		logText = "断言“" + element.getElementData().getName() + "”元素处理后的文本内容（" + text +"）包含"
-				+ (isJudgeAllKey ? "所有" : "部分") + "关键词" + arrayToString(keys);
-		resultText = String.valueOf(result);
-		
+
+		String logText = "断言“%s”元素处理后的文本内容（%s）包含%s关键词：%s";
+		brower.getLogRecord().recordLog(String.format(logText, element.getElementData().getName(), text,
+				(isJudgeAllKey ? "所有" : "部分"), arrayToString(keys)));
+
 		return result;
 	}
-	
+
 	/**
 	 * 用于断言元素的内容与预期的文本一致
+	 * 
 	 * @param element {@link Element}对象
-	 * @param text 需要判断的文本内容
+	 * @param text    需要判断的文本内容
 	 * @return 断言结果
-	 * @throws TimeoutException 元素无法操作时抛出的异常 
-	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常 
+	 * @throws TimeoutException       元素无法操作时抛出的异常
+	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常
 	 */
 	public boolean assertEqualsText(Element element, String text) {
-		//判断是否传入关键词
+		// 判断是否传入关键词
 		text = text == null ? "" : text;
-		
-//		boolean result = judgetText(textEvent.getText(element), true, false, text);	
+
 		boolean result = assertTextContainKey(element, true, text);
-		
-		logText = "断言“" + element.getElementData().getName() + "”元素的文本内容为“" + text + "”";
-		resultText = String.valueOf(result);
-		
+
+		// 由于调用其他断言方法，故需要删除前一条日志
+		brower.getLogRecord().removeLog(1);
+
+		String logText = "断言“%s”元素的文本内容为“%s”";
+		brower.getLogRecord().recordLog(String.format(logText, element.getElementData().getName(), text));
+
 		return result;
 	}
-	
+
 	/**
 	 * 用于断言元素的内容与传入的文本不一致
+	 * 
 	 * @param element {@link Element}对象
-	 * @param text 需要判断的文本内容
+	 * @param text    需要判断的文本内容
 	 * @return 断言结果
-	 * @throws TimeoutException 元素无法操作时抛出的异常 
-	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常 
+	 * @throws TimeoutException       元素无法操作时抛出的异常
+	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常
 	 */
 	public boolean assertNotEqualsText(Element element, String text) {
-		//判断是否传入关键词
+		// 判断是否传入关键词
 		text = text == null ? "" : text;
-		
+
 		boolean result = assertTextNotContainKey(element, true, text);
 		
-		logText = "断言“" + element.getElementData().getName() + "”元素的文本内容不为“" + text + "”";
-		resultText = String.valueOf(result);
-		
+		// 由于调用其他断言方法，故需要删除前一条日志
+		brower.getLogRecord().removeLog(1);
+
+		String logText = "断言“%s”元素的文本内容不为“%s”";
+		brower.getLogRecord().recordLog(String.format(logText, element.getElementData().getName(), text));
+
 		return result;
 	}
-	
+
 	/**
 	 * 断言两个元素内文本一致
-	 * @param referencedElement 参考元素
+	 * 
+	 * @param referencedElement  参考元素
 	 * @param comparativeElement 比较元素
 	 * @return 断言结果
 	 * 
-	 * @throws TimeoutException 元素无法操作时抛出的异常 
-	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常 
+	 * @throws TimeoutException       元素无法操作时抛出的异常
+	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常
 	 */
 	public boolean assertEqualsElementText(Element referencedElement, Element comparativeElement) {
 		boolean result = assertTextContainKey(referencedElement, true, textEvent.getText(comparativeElement));
-	
-		logText = "断言“" + referencedElement.getElementData().getName() + "”元素的文本内容与“" 
-					+ comparativeElement + "”元素的文本内容一致";
-		resultText = String.valueOf(result);
-		
+
+		// 由于调用其他断言方法，故需要删除前一条日志
+		brower.getLogRecord().removeLog(3);
+
+		String logText = "断言“%s”元素的文本内容与“%s”元素的文本内容一致";
+		brower.getLogRecord().recordLog(String.format(logText, referencedElement.getElementData().getName(),
+				comparativeElement.getElementData().getName()));
+
 		return result;
 	}
-	
+
 	/**
 	 * 断言两个元素内文本不一致
-	 * @param referencedElement 参考元素
+	 * 
+	 * @param referencedElement  参考元素
 	 * @param comparativeElement 比较元素
 	 * @return 断言结果
 	 * 
-	 * @throws TimeoutException 元素无法操作时抛出的异常 
-	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常 
+	 * @throws TimeoutException       元素无法操作时抛出的异常
+	 * @throws NoSuchElementException 元素不存在或下标不正确时抛出的异常
 	 */
 	public boolean assertNotEqualsElementText(Element referencedElement, Element comparativeElement) {
 		boolean result = assertTextNotContainKey(referencedElement, true, textEvent.getText(comparativeElement));
-	
-		logText = "断言“" + referencedElement.getElementData().getName() + "”元素的文本内容与“" 
-					+ comparativeElement + "”元素的文本内容不一致";
-		resultText = String.valueOf(result);
-		
+
+		// 由于调用其他断言方法，故需要删除前一条日志
+		brower.getLogRecord().removeLog(2);
+
+		String logText = "断言“%s”元素的文本内容与“%s”元素的文本内容不一致";
+		brower.getLogRecord().recordLog(String.format(logText, referencedElement.getElementData().getName(),
+				comparativeElement.getElementData().getName()));
+
 		return result;
 	}
-	
+
 	/**
 	 * 断言元素存在
+	 * 
 	 * @param element {@link Element}对象
 	 * @return 断言结果
 	 */
 	public boolean assertExistElement(Element element) {
 		boolean result = isExistElement(element);
-		
-		logText = "断言“" + element.getElementData().getName() + "”元素存在";
-		resultText = String.valueOf(result);
-		
+
+		String logText = "断言“%s”元素存在";
+		brower.getLogRecord().recordLog(String.format(logText, element.getElementData().getName()));
+
 		return result;
 	}
-	
+
 	/**
 	 * 断言元素不存在
+	 * 
 	 * @param element {@link Element}对象
 	 * @return 断言结果
 	 */
 	public boolean assertNotExistElement(Element element) {
 		boolean result = !isExistElement(element);
-		
-		logText = "断言“" + element.getElementData().getName() + "”元素不存在";
-		resultText = String.valueOf(result);
-		
+
+		String logText = "断言“%s”元素不存在";
+		brower.getLogRecord().recordLog(String.format(logText, element.getElementData().getName()));
+
 		return result;
 	}
-	
+
 	/**
 	 * 断言两元素中的数字内容，按照{@link CompareNumberType}指定的比较类型进行比较。若断言的
 	 * 元素文本内容存在非数字字符时，则直接断言失败
-	 * @param referencedElement 参考元素
-	 * @param compareNumberType 比较方式{@link CompareNumberType}枚举类
+	 * 
+	 * @param referencedElement  参考元素
+	 * @param compareNumberType  比较方式{@link CompareNumberType}枚举类
 	 * @param comparativeElement 比较元素
 	 * @return 断言结果
 	 */
-	public boolean assertElementsNumber(Element referencedElement, CompareNumberType compareNumberType, Element comparativeElement) {
-		logText = "断言“" + referencedElement.getElementData().getName() + "”元素数字内容" 
-					+ compareNumberType.getLogText() 
-					+ "“" + comparativeElement.getElementData().getName() + "”元素数字内容";
-		boolean result = assertNumber(referencedElement, compareNumberType, Double.valueOf(textEvent.getText(comparativeElement)));
+	public boolean assertElementsNumber(Element referencedElement, CompareNumberType compareNumberType,
+			Element comparativeElement) {
+		String logText = "断言“%s”元素数字内容%s“%s”元素数字内容";
+		brower.getLogRecord().recordLog(String.format(logText, referencedElement.getElementData().getName(),
+				compareNumberType.getLogText(), comparativeElement.getElementData().getName()));
 		
-		resultText = String.valueOf(result);
+		// 当判定元素为非数字文本时，则直接返回false
+		boolean result = false;
+		try {
+			result = assertNumber(referencedElement, compareNumberType,
+					Double.valueOf(textEvent.getText(comparativeElement)));
+			
+			// 删除获取文本和调用assertNumber方法的日志
+			brower.getLogRecord().removeLog(2);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		
 		return result;
 	}
-	
+
 	/**
 	 * 断言元素中的数字内容与指定的数字按照{@link CompareNumberType}指定的比较类型进行比较。若断言的
 	 * 元素文本内容为非数字字符时，则直接断言失败
 	 * 
-	 * @param element {@link Element}对象
+	 * @param element           {@link Element}对象
 	 * @param compareNumberType 比较方式{@link CompareNumberType}枚举类
-	 * @param compareNumber 预期数字
+	 * @param compareNumber     预期数字
 	 * @return 断言结果
 	 */
 	public boolean assertNumber(Element element, CompareNumberType compareNumberType, double compareNumber) {
-		logText = "断言“" + element.getElementData().getName() + "”元素数字内容" + compareNumberType.getLogText() + compareNumber;
+		String logText = "断言“%s”元素数字内容%s%f";
+		brower.getLogRecord().recordLog(String.format(logText, element.getElementData().getName(),
+				compareNumberType.getLogText(), compareNumber));
+
 		boolean result = AssertStringUtil.assertNumber(textEvent.getText(element), compareNumberType, compareNumber);
- 		resultText = String.valueOf(result);
+		// 删除获取文本的日志
+		brower.getLogRecord().removeLog(1);
+		
 		return result;
 	}
 }
