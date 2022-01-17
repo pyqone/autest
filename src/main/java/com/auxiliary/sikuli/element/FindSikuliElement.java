b00769865d73b86ca8e2b646097df6128851e688
pyqone <465615774@qq.com>
2022-01-17 17:31:58 +0800

    完成元素查找方法编写

--- src/main/java/com/auxiliary/sikuli/element/FindSikuliElement.java
+++ src/main/java/com/auxiliary/sikuli/element/FindSikuliElement.java
@@ -2,8 +2,10 @@ package com.auxiliary.sikuli.element;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.sikuli.script.Match;
 import org.sikuli.script.Region;
@@ -61,7 +63,7 @@ public class FindSikuliElement {
     }
 
     /**
-     * 该方法用于根据设置的识别范围，查找对应名称的元素
+     * 该方法用于根据设置的识别范围，查找对应名称的元素。若界面上识别到多个结果，则取第1个作为目标元素。
      *
      * @param name 元素名称
      * @param linkKeys 外链词语
@@ -69,7 +71,36 @@ public class FindSikuliElement {
      * @since autest 3.0.0
      * @throws TimeoutException 当元素查找超时时，抛出的异常
      */
-    public SikuliElement find(String name, String...linkKeys) {
+    public SikuliElement findElement(String name, String...linkKeys) {
+        return findElement(name, 1, linkKeys);
+    }
+
+    /**
+     * 该方法用于根据设置的识别范围，查找对应名称的元素，并返回指定下标的元素
+     * <p><b>注意：</b>元素下标传入元素的真实下标，例如，当在界面识别到3个相同的元素时，需要取第2个作为目标元素，则下标传入2。
+     * 另外，下标可以传入负数，表示从后向前遍历</p>
+     *
+     * @param name 元素名称
+     * @param linkKeys 外链词语
+     * @return 查找到的元素类对象
+     * @since autest 3.0.0
+     * @throws TimeoutException 当元素查找超时时，抛出的异常
+     */
+    public SikuliElement findElement(String name, int index, String...linkKeys) {
+        List<SikuliElement> elementList = findAllElement(name, linkKeys);
+        return elementList.get(disposeElementIndex(index, elementList.size()));
+    }
+
+    /**
+     * 该方法用于根据设置的识别范围，查找对应名称的元素所有元素，并返回目标元素集合
+     *
+     * @param name 元素名称
+     * @param linkKeys 外链词语
+     * @return 查找到的元素类对象
+     * @since autest3.0.0
+     * @throws TimeoutException 当元素查找超时时，抛出的异常
+     */
+    public List<SikuliElement> findAllElement(String name, String...linkKeys) {
         // 查找元素信息
         List<ElementLocationInfo> infoList = locationFile.getElementLocationList(name);
         // 根据外链词信息，将包含占位符的内容进行替换
@@ -87,9 +118,9 @@ public class FindSikuliElement {
             long startTime = System.currentTimeMillis();
             // 将infoList中存储的所有元素遍历一次，直至界面上出现相应元素为止
             for (ElementLocationInfo info : infoList) {
-                Match element = region.exists(info.getPattern(), 0.0);
-                if (element != null) {
-                    return new SikuliElement(element);
+                List<Match> matchList = region.findAllList(info.getPattern());
+                if (matchList.size() != 0) {
+                    return matchList.stream().map(SikuliElement::new).collect(Collectors.toList());
                 }
             }
 
@@ -100,6 +131,42 @@ public class FindSikuliElement {
         // 若循环结束仍未查找到元素，则抛出超时异常
         // TODO 当前异常说明中的单位也写死，后续优化需要注意
         throw new TimeoutException(String.format("元素“%s”%d%s内未出现在指定的识别区域内", name, locationFile.getWaitTime(name), "秒"));
+    }
+
+    /**
+     * 该方法用于处理传入的元素下标内容，下标传入元素真实下标，可传入负数，表示向后遍历
+     * <ol>
+     * <li>当下标绝对值大于元素总数量时，若下标为正数，则返回总数减1；反之，则返回0</li>
+     * <li>当下标为正数，且小于总数时，则返回下标减1</li>
+     * <li>当下标为负数，且下标绝对值小于总数时，则返回总数加上下标的值（即总数 - 下标绝对值）</li>
+     * <li>当下标为0时，则在元素总数范围内，随机返回一个数</li>
+     * </ol>
+     *
+     * @param index 传入的下标
+     * @param length 元素总数量
+     * @return 处理后的元素下标
+     * @since autest 3.0.0
+     */
+    private int disposeElementIndex(int index, int length) {
+        // 判断元素下标是否超出范围，若下标为负数，则返回0；否则，返回元素总个数减1
+        if (Math.abs(index) >= length) {
+            if (index > 0) {
+                return length - 1;
+            } else {
+               return 0;
+            }
+        }
+
+        // 判断index的值，若大于0，则从前向后遍历，若小于0，则从后往前遍历，若等于0，则随机输入
+        if (index > 0) {
+            // 选择元素，正数的选项值从1开始，故需要减小1
+            return index - 1;
+        } else if (index < 0) {
+            // 选择元素，由于index为负数，则长度加上选项值即可得到需要选择的选项
+            return length + index;
+        } else {
+            return new Random().nextInt(length);
+        }
     }
 
     /**
