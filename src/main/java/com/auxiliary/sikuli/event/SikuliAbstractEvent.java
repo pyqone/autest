65de1e1787b8d6adfe202a169f9fe662e6d6c173
pyqone <465615774@qq.com>
2022-01-24 16:46:22 +0800

    完成鼠标事件的开发

--- src/main/java/com/auxiliary/sikuli/event/SikuliAbstractEvent.java
+++ src/main/java/com/auxiliary/sikuli/event/SikuliAbstractEvent.java
@@ -7,14 +7,14 @@ import org.sikuli.script.Match;
 
 import com.auxiliary.selenium.tool.ActionLogRecord;
 import com.auxiliary.sikuli.element.FindSikuliElement;
+import com.auxiliary.sikuli.element.SikuliElement;
 
 /**
  * <p>
  * <b>文件名：</b>SikuliAbstractEvent.java
  * </p>
  * <p>
- * <b>用途：</b>
- * 定义所有sikuli事件类包含的基本方法
+ * <b>用途：</b> 定义所有sikuli事件类包含的基本方法
  * </p>
  * <p>
  * <b>编码时间：</b>2022年1月19日 上午8:02:37
@@ -43,6 +43,7 @@ public abstract class SikuliAbstractEvent {
 
     /**
      * 构造对象，并初始化日志记录工具
+     *
      * @since autest 3.0.0
      */
     public SikuliAbstractEvent() {
@@ -62,7 +63,7 @@ public abstract class SikuliAbstractEvent {
     /**
      * 该方法用于设置记录操作日志的工具，并根据参数，决定是否保留当前已有的日志
      *
-     * @param log 日志记录工具类{@link ActionLogRecord}对象
+     * @param log          日志记录工具类{@link ActionLogRecord}对象
      * @param isSaveOldLog 是否保存原有日志
      * @since autest 3.0.0
      */
@@ -91,9 +92,11 @@ public abstract class SikuliAbstractEvent {
 
     /**
      * 该方法用于记录日志，并根据指定的数字，在记录前删除多余的日志
-     * <p><b>注意：</b>当指定的数字小于等于0时，表示不删除日志</p>
+     * <p>
+     * <b>注意：</b>当指定的数字小于等于0时，表示不删除日志
+     * </p>
      *
-     * @param logText 日志文本
+     * @param logText      日志文本
      * @param removeLogNum 需要移除的日志数量
      * @since autest 3.0.0
      */
@@ -107,21 +110,25 @@ public abstract class SikuliAbstractEvent {
 
     /**
      * 该方法用于在指定的超时时间内，执行相应的操作
-     * <p><b>注意：</b>调用本方法时，在action失败时，需要返回一个null，以此来作为操作超时的循环执行条件，所以当action执行成功后，必须指定一个返回值</p>
+     * <p>
+     * <b>注意：</b>调用本方法时，在action失败时，需要返回一个null，以此来作为操作超时的循环执行条件，所以当action执行成功后，必须指定一个返回值
+     * </p>
      *
-     * @param element {@link Match}类对象
-     * @param action 需要执行的操作
+     * @param element   {@link Match}类对象
+     * @param action    需要执行的操作
      * @param eventName 事件名称
      * @return 操作返回结果
      * @since autest 3.0.0
      */
-    protected Object actionOperate(String eventName, Match element, Function<Match, Object> action) {
+    protected Object actionOperate(String eventName, SikuliElement element, Function<Match, Object> action) {
         long time = operateTime;
         // 在指定时间内，循环调用操作方法，直至操作超时为止
-        while(time > 0) {
+        while (time > 0) {
             long startTime = System.currentTimeMillis();
 
-            Object result = action.apply(element);
+            Object result = action.apply(Optional.ofNullable(element)
+                    .orElseThrow(() -> new ElementOperateException(String.format("为指定元素对象，无法进行“%s”操作", eventName)))
+                    .getElement());
             if (result != null) {
                 return result;
             }
--- src/main/java/com/auxiliary/sikuli/event/SikuliMouseEvent.java
+++ src/main/java/com/auxiliary/sikuli/event/SikuliMouseEvent.java
@@ -2,6 +2,7 @@ package com.auxiliary.sikuli.event;
 
 import java.util.Optional;
 
+import com.auxiliary.sikuli.SikuliToolsExcepton;
 import com.auxiliary.sikuli.element.FindSikuliElement;
 import com.auxiliary.sikuli.element.SikuliElement;
 import com.auxiliary.sikuli.element.TimeoutException;
@@ -11,8 +12,7 @@ import com.auxiliary.sikuli.element.TimeoutException;
  * <b>文件名：</b>SikuliMouseEvent.java
  * </p>
  * <p>
- * <b>用途：</b>
- * 封装sikuli工具的鼠标事件，可设置{@link FindSikuliElement}类对象，用以元素名称来进行操作
+ * <b>用途：</b> 封装sikuli工具的鼠标事件，可设置{@link FindSikuliElement}类对象，用以元素名称来进行操作
  * </p>
  * <p>
  * <b>编码时间：</b>2022年1月21日 上午8:03:49
@@ -28,6 +28,7 @@ import com.auxiliary.sikuli.element.TimeoutException;
 public class SikuliMouseEvent extends SikuliAbstractEvent {
     /**
      * 构造对象
+     *
      * @since autest 3.0.0
      */
     public SikuliMouseEvent() {
@@ -44,7 +45,7 @@ public class SikuliMouseEvent extends SikuliAbstractEvent {
     public void click(SikuliElement element) {
         String eventName = "鼠标左键单击";
 
-        actionOperate(eventName, element.getElement(), match -> {
+        actionOperate(eventName, element, match -> {
             // 根据点击事件的返回值是否为0判断操作是否成功
             if (match.click() != 0) {
                 return "";
@@ -53,21 +54,21 @@ public class SikuliMouseEvent extends SikuliAbstractEvent {
             }
         });
 
-        recordLog(String.format("%s%s元素", eventName, element.getName()), 0);
+        recordLog(String.format("%s“%s”元素", eventName, element.getName()), 0);
     }
 
     /**
      * 该方法用于根据元素信息，通过指定的元素查找类，查找到目标元素后，使用鼠标左键单击目标元素
      *
      * @param elementName 元素名称
-     * @param index 多元素时的下标，允许传入负数，参考{@link FindSikuliElement#findElement(String, int, String...)}方法
-     * @param keys 外链词语
+     * @param index       多元素时的下标，允许传入负数，参考{@link FindSikuliElement#findElement(String, int, String...)}方法
+     * @param keys        外链词语
      * @since autest 3.0.0
      * @throws ElementOperateException 未指定元素查找类时，抛出的异常
-     * @throws TimeoutException 元素查找超时时，抛出的异常
+     * @throws TimeoutException        元素查找超时时，抛出的异常
      * @throws OperateTimeoutException 操作超时时，抛出的异常
      */
-    public void click(String elementName, int index, String...keys) {
+    public void click(String elementName, int index, String... keys) {
         if (Optional.ofNullable(find).isPresent()) {
             click(find.findElement(elementName, index, keys));
         } else {
@@ -79,13 +80,13 @@ public class SikuliMouseEvent extends SikuliAbstractEvent {
      * 该方法用于根据元素信息，通过指定的元素查找类，查找到目标元素后，使用鼠标左键单击目标元素
      *
      * @param elementName 元素名称
-     * @param keys 外链词语
+     * @param keys        外链词语
      * @since autest 3.0.0
      * @throws ElementOperateException 未指定元素查找类时，抛出的异常
-     * @throws TimeoutException 元素查找超时时，抛出的异常
+     * @throws TimeoutException        元素查找超时时，抛出的异常
      * @throws OperateTimeoutException 操作超时时，抛出的异常
      */
-    public void click(String elementName, String...keys) {
+    public void click(String elementName, String... keys) {
         click(elementName, 1, keys);
     }
 
@@ -99,7 +100,7 @@ public class SikuliMouseEvent extends SikuliAbstractEvent {
     public void doubleClick(SikuliElement element) {
         String eventName = "鼠标左键双击";
 
-        actionOperate(eventName, element.getElement(), match -> {
+        actionOperate(eventName, element, match -> {
             // 根据点击事件的返回值是否为0判断操作是否成功
             if (match.doubleClick() != 0) {
                 return "";
@@ -108,21 +109,21 @@ public class SikuliMouseEvent extends SikuliAbstractEvent {
             }
         });
 
-        recordLog(String.format("%S%s元素", eventName, element.getName()), 0);
+        recordLog(String.format("%S“%s”元素", eventName, element.getName()), 0);
     }
 
     /**
      * 该方法用于根据元素信息，通过指定的元素查找类，查找到目标元素后，使用鼠标左键双击目标元素
      *
      * @param elementName 元素名称
-     * @param index 多元素时的下标，允许传入负数，参考{@link FindSikuliElement#findElement(String, int, String...)}方法
-     * @param keys 外链词语
+     * @param index       多元素时的下标，允许传入负数，参考{@link FindSikuliElement#findElement(String, int, String...)}方法
+     * @param keys        外链词语
      * @since autest 3.0.0
      * @throws ElementOperateException 未指定元素查找类时，抛出的异常
-     * @throws TimeoutException 元素查找超时时，抛出的异常
+     * @throws TimeoutException        元素查找超时时，抛出的异常
      * @throws OperateTimeoutException 操作超时时，抛出的异常
      */
-    public void doubleClick(String elementName, int index, String...keys) {
+    public void doubleClick(String elementName, int index, String... keys) {
         if (Optional.ofNullable(find).isPresent()) {
             doubleClick(find.findElement(elementName, index, keys));
         } else {
@@ -134,13 +135,13 @@ public class SikuliMouseEvent extends SikuliAbstractEvent {
      * 该方法用于根据元素信息，通过指定的元素查找类，查找到目标元素后，使用鼠标左键双击目标元素
      *
      * @param elementName 元素名称
-     * @param keys 外链词语
+     * @param keys        外链词语
      * @since autest 3.0.0
      * @throws ElementOperateException 未指定元素查找类时，抛出的异常
-     * @throws TimeoutException 元素查找超时时，抛出的异常
+     * @throws TimeoutException        元素查找超时时，抛出的异常
      * @throws OperateTimeoutException 操作超时时，抛出的异常
      */
-    public void doubleClick(String elementName, String...keys) {
+    public void doubleClick(String elementName, String... keys) {
         doubleClick(elementName, 1, keys);
     }
 
@@ -154,7 +155,7 @@ public class SikuliMouseEvent extends SikuliAbstractEvent {
     public void rightClick(SikuliElement element) {
         String eventName = "鼠标右键单击";
 
-        actionOperate(eventName, element.getElement(), match -> {
+        actionOperate(eventName, element, match -> {
             // 根据点击事件的返回值是否为0判断操作是否成功
             if (match.rightClick() != 0) {
                 return "";
@@ -163,21 +164,21 @@ public class SikuliMouseEvent extends SikuliAbstractEvent {
             }
         });
 
-        recordLog(String.format("%s%s元素", eventName, element.getName()), 0);
+        recordLog(String.format("%s“%s”元素", eventName, element.getName()), 0);
     }
 
     /**
      * 该方法用于根据元素信息，通过指定的元素查找类，查找到目标元素后，使用鼠标右键单击目标元素
      *
      * @param elementName 元素名称
-     * @param index 多元素时的下标，允许传入负数，参考{@link FindSikuliElement#findElement(String, int, String...)}方法
-     * @param keys 外链词语
+     * @param index       多元素时的下标，允许传入负数，参考{@link FindSikuliElement#findElement(String, int, String...)}方法
+     * @param keys        外链词语
      * @since autest 3.0.0
      * @throws ElementOperateException 未指定元素查找类时，抛出的异常
-     * @throws TimeoutException 元素查找超时时，抛出的异常
+     * @throws TimeoutException        元素查找超时时，抛出的异常
      * @throws OperateTimeoutException 操作超时时，抛出的异常
      */
-    public void rightClick(String elementName, int index, String...keys) {
+    public void rightClick(String elementName, int index, String... keys) {
         if (Optional.ofNullable(find).isPresent()) {
             rightClick(find.findElement(elementName, index, keys));
         } else {
@@ -189,13 +190,90 @@ public class SikuliMouseEvent extends SikuliAbstractEvent {
      * 该方法用于根据元素信息，通过指定的元素查找类，查找到目标元素后，使用鼠标右键单击目标元素
      *
      * @param elementName 元素名称
-     * @param keys 外链词语
+     * @param keys        外链词语
      * @since autest 3.0.0
      * @throws ElementOperateException 未指定元素查找类时，抛出的异常
-     * @throws TimeoutException 元素查找超时时，抛出的异常
+     * @throws TimeoutException        元素查找超时时，抛出的异常
      * @throws OperateTimeoutException 操作超时时，抛出的异常
      */
-    public void rightClick(String elementName, String...keys) {
+    public void rightClick(String elementName, String... keys) {
         rightClick(elementName, 1, keys);
+    }
+
+    /**
+     * 该方法用于连续指定次数的鼠标左键单击事件
+     *
+     * @param clickCount    点击次数
+     * @param sleepInMillis 每次之间的时间间隔（单位：毫秒）
+     * @param element       元素类对象
+     * @since autest 3.0.0
+     * @throws OperateTimeoutException 操作超时时，抛出的异常
+     */
+    public void continuousClick(int clickCount, long sleepInMillis, SikuliElement element) {
+        // 判断点击次数是否小于0
+        if (clickCount < 1) {
+            throw new ElementOperateException(
+                    String.format("点击次数%d小于1次，无法对“%s”元素进行连续点击", clickCount, element.getName()));
+        }
+
+        // 判断间隔时间是否小于0
+        if (sleepInMillis < 0L) {
+            sleepInMillis = 0L;
+        }
+
+        // 循环，根据点击次数点击元素
+        for (int i = 0; i < clickCount; i++) {
+            try {
+                click(element);
+                try {
+                    Thread.sleep(sleepInMillis);
+                } catch (InterruptedException e) {
+                    continue;
+                }
+            } catch (SikuliToolsExcepton e) {
+                // 若点击事件存在异常，则删除已存储的日志
+                log.removeLog(i + 1);
+                throw e;
+            }
+        }
+
+        recordLog(String.format("鼠标左键单击“%s”元素%d次，每次间隔%d毫秒", element.getName(), clickCount, sleepInMillis), clickCount);
+    }
+
+    /**
+     * 该方法用于根据元素信息，通过指定的元素查找类，查找到目标元素后，连续指定次数的鼠标左键单击事件
+     *
+     * @param clickCount    点击次数
+     * @param sleepInMillis 每次之间的时间间隔（单位：毫秒）
+     * @param elementName   元素名称
+     * @param index         多元素时的下标，允许传入负数，参考{@link FindSikuliElement#findElement(String, int, String...)}方法
+     * @param keys          外链词语
+     * @since autest 3.0.0
+     * @throws ElementOperateException 未指定元素查找类时，抛出的异常
+     * @throws TimeoutException        元素查找超时时，抛出的异常
+     * @throws OperateTimeoutException 操作超时时，抛出的异常
+     */
+    public void continuousClick(int clickCount, long sleepInMillis, String elementName, int index, String... keys) {
+        if (Optional.ofNullable(find).isPresent()) {
+            continuousClick(clickCount, sleepInMillis, find.findElement(elementName, index, keys));
+        } else {
+            throw new ElementOperateException("未指定元素查找类对象，无法通过元素名称查找指定的元素");
+        }
+    }
+
+    /**
+     * 该方法用于根据元素信息，通过指定的元素查找类，查找到目标元素后，连续指定次数的鼠标左键单击事件
+     *
+     * @param clickCount    点击次数
+     * @param sleepInMillis 每次之间的时间间隔（单位：毫秒）
+     * @param elementName   元素名称
+     * @param keys          外链词语
+     * @since autest 3.0.0
+     * @throws ElementOperateException 未指定元素查找类时，抛出的异常
+     * @throws TimeoutException        元素查找超时时，抛出的异常
+     * @throws OperateTimeoutException 操作超时时，抛出的异常
+     */
+    public void continuousClick(int clickCount, long sleepInMillis, String elementName, String... keys) {
+        continuousClick(clickCount, sleepInMillis, elementName, 1, keys);
     }
 }
