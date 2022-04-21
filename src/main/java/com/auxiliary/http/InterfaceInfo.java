81e5d242b42e9fad0430ea464f6c07ad1e31656c
pyqone <465615774@qq.com>
2022-04-21 08:09:18 +0800

    添加识别url方法

--- src/main/java/com/auxiliary/http/InterfaceInfo.java
+++ src/main/java/com/auxiliary/http/InterfaceInfo.java
@@ -55,6 +55,31 @@ public class InterfaceInfo implements Cloneable {
     protected final String DEFAULT_CHARSETNAME = "UTF-8";
 
     /**
+     * 定义接口主机与协议间分隔的符号
+     */
+    protected final String SYMBOL_SPLIT_PROTOCOL = "://";
+    /**
+     * 定义接口主机与主机间的分隔符号
+     */
+    protected final String SYMBOL_SPLIT_PORT = ":";
+    /**
+     * 定义接口路径与接口主机间的分隔符号
+     */
+    protected final String SYMBOL_SPLIT_PATH = "/";
+    /**
+     * 定义接口起始参数与接口路径间的分隔符号
+     */
+    protected final String SYMBOL_SPLIT_START_PARAM = "?";
+    /**
+     * 定义接口参数键值之间的分隔符号
+     */
+    protected final String SYMBOL_SPLIT_PARAM_VALUE = "=";
+    /**
+     * 定义接口参数与参数间的分隔符号
+     */
+    protected final String SYMBOL_SPLIT_PARAM = "&";
+
+    /**
      * 接口协议，默认http
      */
     private String protocol = DEFAULT_PROTOCOL;
@@ -114,15 +139,15 @@ public class InterfaceInfo implements Cloneable {
      * @since autest 3.3.0
      */
     public void setProtocol(String protocol) {
-        this.protocol = Optional.ofNullable(protocol).filter(p -> !p.isEmpty()).map(p -> {
-            // 判断协议内容末尾是否包含相应的符号，若不包含，则添加相应的符号
-            String symbol = "://";
-            if (p.lastIndexOf(symbol) != p.length() - symbol.length()) {
-                p += symbol;
-            }
-
-            return p;
-        }).orElseGet(() -> DEFAULT_PROTOCOL);
+        // 过滤协议为空以及只包含协议符号的情况
+        this.protocol = Optional.ofNullable(protocol).filter(p -> !p.isEmpty()).filter(p -> p.matches(".+:\\/\\/"))
+                .map(p -> {
+                    // 判断协议内容末尾是否包含相应的符号，若不包含，则添加相应的符号
+                    if (p.lastIndexOf(SYMBOL_SPLIT_PROTOCOL) != p.length() - SYMBOL_SPLIT_PROTOCOL.length()) {
+                        p += SYMBOL_SPLIT_PROTOCOL;
+                    }
+                    return p;
+                }).orElseGet(() -> DEFAULT_PROTOCOL);
     }
 
     /**
@@ -144,10 +169,9 @@ public class InterfaceInfo implements Cloneable {
     public void setHost(String host) {
         this.host = Optional.ofNullable(host).filter(h -> !h.isEmpty()).map(h -> {
             // 判断主机或域名最后一位是否为/，若是，则进行去除
-            if (h.lastIndexOf("/") == h.length() - 1) {
+            if (h.lastIndexOf(SYMBOL_SPLIT_PATH) == h.length() - 1) {
                 h = h.substring(0, h.length() - 1);
             }
-
             return h;
         }).orElseGet(() -> DEFAULT_HOST);
     }
@@ -195,16 +219,78 @@ public class InterfaceInfo implements Cloneable {
     public void setPath(String path) {
         this.path = Optional.ofNullable(path).map(p -> {
             // 判断接口第一位是否为/，若不是，则添加
-            if (p.indexOf("/") != 0) {
-                p = "/" + p;
+            if (p.indexOf(SYMBOL_SPLIT_PATH) != 0) {
+                p = SYMBOL_SPLIT_PATH + p;
             }
             // 判断接口路径的最后一位是否为/，若是，则进行去除
-            if (p.lastIndexOf("/") == p.length() - 1) {
+            if (p.lastIndexOf(SYMBOL_SPLIT_PATH) == p.length() - 1) {
                 p = p.substring(0, p.length() - 1);
             }
-
             return p;
         }).orElseGet(() -> "");
+    }
+
+    /**
+     * 该方法用于解析传入的url，并根据url的分隔规则，将接口信息进行识别，并存入到相应的参数中
+     * <p>
+     * 方法将传入的url按如下的顺序进行识别：
+     * <ol>
+     * <li>当识别到“?”符号时，则将其后的内容识别为参数表达式存储至参数列表中</li>
+     * <li>当识别到“://”符号时，则将其前的数据识别为协议</li>
+     * <li>当识别到“/”符号时，则将其后的内容识别为接口地址</li>
+     * <li>当识别到“:”符号，则将其后的内容识别为端口号</li>
+     * <li>识别完上述内容后，则将剩余的字符串识别为主机IP或域名</li>
+     * </ol>
+     * </p>
+     * <p>
+     * <b>注意：</b>该方法亦可仅传入url的一部分，用于设置已存储的接口信息，但传入非主机内容时，需要带该参数的分隔符号，
+     * 否则传入的内容将会被识别为主机或域名（例如，仅传入ftp协议时，则传参必须是“ftp://”）
+     * </p>
+     *
+     * @param url 接口请求url
+     * @return 类本身
+     * @since autest 3.3.0
+     */
+    public InterfaceInfo analysisUrl(String url) {
+        if (url == null || url.isEmpty()) {
+            return this;
+        }
+
+        // 将传入接口的所有中文符号替换成英文符号，并存储
+        StringBuilder urlText = new StringBuilder(
+                url.replaceAll("：", SYMBOL_SPLIT_PORT).replaceAll("？", SYMBOL_SPLIT_START_PARAM).replaceAll("。·", "."));
+        int index = -1;
+
+        // 解析url中的参数，若存在参数分隔符，则在urlText去除，并将表达式传入相应的内容中
+        if ((index = urlText.indexOf(SYMBOL_SPLIT_START_PARAM)) > -1) {
+            addParam(urlText.substring(index));
+            urlText.delete(index, urlText.length());
+        }
+
+        // 解析协议参数，若存在协议分隔符，则在urlText去除，并将协议传入相应的内容中
+        if ((index = urlText.indexOf(SYMBOL_SPLIT_PROTOCOL)) > -1) {
+            // 从第一位起，分离协议，并存储分离后的内容
+            setProtocol(urlText.substring(0, index + SYMBOL_SPLIT_PROTOCOL.length()));
+            urlText.delete(0, index + SYMBOL_SPLIT_PROTOCOL.length());
+        }
+
+        // 解析接口路径参数，若存在接口路径分隔符，则在urlText去除，并将路径传入相应的内容中
+        if ((index = urlText.indexOf(SYMBOL_SPLIT_PATH)) > -1) {
+            setPath(urlText.substring(index));
+            urlText.delete(index, urlText.length());
+        }
+
+        // 解析主机端口，若存在主机端口分隔符，则在urlText去除，并将端口转换后传入相应的内容中
+        if ((index = urlText.indexOf(SYMBOL_SPLIT_PORT)) > -1) {
+            try {
+                setPort(Integer.valueOf(urlText.substring(index).replaceAll(SYMBOL_SPLIT_PORT, "")));
+                urlText.delete(index, urlText.length());
+            } catch (NumberFormatException e) {
+            }
+        }
+
+        setHost(urlText.toString());
+        return this;
     }
 
     /**
@@ -275,9 +361,11 @@ public class InterfaceInfo implements Cloneable {
      */
     public void addParam(String expression) {
         // 过滤不符合正则的表达式，之后按照分隔符号切分，得到每个参数的表达式后，再按照参数与值的符号切分
-        Optional.ofNullable(expression).filter(exp -> exp.matches("\\w+=\\w*(&\\w+=\\w*)*")).map(exp -> exp.split("&"))
-                .map(Arrays::stream).ifPresent(expStream -> expStream.filter(pramExp -> pramExp.contains("="))
-                        .map(pramExp -> pramExp.split("=")).forEach(params -> {
+        Optional.ofNullable(expression).filter(exp -> exp.matches("[\\?？]?\\w+=\\w*(&\\w+=\\w*)*"))
+                .map(exp -> (exp.indexOf("?") == 0 || exp.indexOf("？") == 0) ? exp.substring(1) : exp)
+                .map(exp -> exp.split(SYMBOL_SPLIT_PARAM)).map(Arrays::stream)
+                .ifPresent(expStream -> expStream.filter(pramExp -> pramExp.contains(SYMBOL_SPLIT_PARAM_VALUE))
+                        .map(pramExp -> pramExp.split(SYMBOL_SPLIT_PARAM_VALUE)).forEach(params -> {
                             if (params.length == 1) {
                                 addParam(params[0], "");
                             } else {
@@ -496,11 +584,11 @@ public class InterfaceInfo implements Cloneable {
      */
     public String toUrlString() {
         // 拼接接口参数信息
-        StringJoiner paramInfo = new StringJoiner("&");
-        paramMap.forEach((k, v) -> paramInfo.add(String.format("%s=%s", k, v)));
+        StringJoiner paramInfo = new StringJoiner(SYMBOL_SPLIT_PARAM);
+        paramMap.forEach((k, v) -> paramInfo.add(String.format("%s%s%s", k, SYMBOL_SPLIT_PARAM_VALUE, v)));
 
-        return String.format("%s%s:%d%s%s", protocol, host, port, path,
-                (paramInfo.length() == 0 ? "?" + paramInfo.toString() : ""));
+        return String.format("%s%s%s%s%s", protocol, host, (port == 80 ? "" : (SYMBOL_SPLIT_PORT + port)), path,
+                (paramInfo.length() != 0 ? (SYMBOL_SPLIT_START_PARAM + paramInfo.toString()) : ""));
     }
 
     @Override
