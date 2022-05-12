c7b3eeb5000636dcee95ce4814ceaf1a3722071a
pyqone <465615774@qq.com>
2022-05-12 10:22:46 +0800

    添加处理公式方法

--- src/main/java/com/auxiliary/http/EasyHttp.java
+++ src/main/java/com/auxiliary/http/EasyHttp.java
@@ -1,9 +1,15 @@
 package com.auxiliary.http;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.auxiliary.datadriven.DataFunction;
 import com.auxiliary.tool.common.Entry;
+import com.auxiliary.tool.regex.ConstType;
+import com.auxiliary.tool.regex.RegexType;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -34,6 +40,28 @@ import okhttp3.Response;
  * @since autest 3.3.0
  */
 public class EasyHttp {
+    /**
+     * 存储提词内容
+     */
+    private HashMap<String, String> extractMap = new HashMap<>(ConstType.DEFAULT_MAP_SIZE);
+    /**
+     * 存储公式内容
+     */
+    private HashMap<String, DataFunction> functionMap = new HashMap<>(ConstType.DEFAULT_MAP_SIZE);
+
+    /**
+     * 占位符标记
+     */
+    private final String FUNCTION_SIGN = "@\\{.+?\\}";
+    /**
+     * 占位符起始符号
+     */
+    private final String FUNCTION_START_SIGN = "@\\{";
+    /**
+     * 占位符结束符号
+     */
+    private final String FUNCTION_END_SIGN = "\\}";
+
     /**
      * 该方法用于根据接口信息，对接口进行请求，并返回响应内容
      *
@@ -112,4 +140,65 @@ public class EasyHttp {
         }
     }
 
+    /**
+     * 该方法用于对所有可以使用公式的文本进行处理，返回处理后的文本
+     *
+     * @param content 待替换文本
+     * @return 处理后的文本
+     * @since autest 3.3.0
+     */
+    private String disposeContent(String content) {
+        // 判断需要才处理的内容是否为空或是否包含公式标志，若存在，则返回content
+        if (content == null || content.isEmpty() || !content.matches(FUNCTION_SIGN)) {
+            return content;
+        }
+
+        // 通过函数标志对文本中的函数或方法进行提取
+        Matcher m = Pattern.compile(FUNCTION_SIGN).matcher(content);
+        while (m.find()) {
+            // 去除标记符号，获取关键词
+            String signKey = m.group();
+            String key = signKey.replaceAll(FUNCTION_START_SIGN, "").replaceAll(FUNCTION_END_SIGN, "");
+
+            // 判断关键词是否为已存储的词语
+            if (extractMap.containsKey(key)) {
+                content.replaceAll(disposeKey(signKey), extractMap.get(key));
+                continue;
+            }
+
+            // 若不是存储的词语，则判断关键词是否符合公式集合中的内容，符合公式，则使用公式对内容进行替换
+            for (String funKey : functionMap.keySet()) {
+                if (key.matches(funKey)) {
+                    content.replaceAll(disposeKey(signKey), functionMap.get(key).apply(key));
+                    break;
+                }
+            }
+
+        }
+
+        return content;
+    }
+
+    /**
+     * 该方法用于处理需要替换的关键词内容，在需要转义的字符前添加反斜杠，避免在替换词语时无法被正则识别
+     *
+     * @param key 关键词
+     * @return 添加转义符后的关键词
+     * @since autest 3.3.0
+     */
+    private String disposeKey(String key) {
+        StringBuilder newKey = new StringBuilder();
+        // 对当前字符串逐字遍历
+        for (int i = 0; i < key.length(); i++) {
+            String charStr = key.substring(i, i + 1);
+            // 若遍历的字符为正则中需要转义的特殊字符，则对该内容进行转义
+            if (charStr.matches(RegexType.REGEX_ESC.getRegex())) {
+                newKey.append("\\" + charStr);
+            } else {
+                newKey.append(charStr);
+            }
+        }
+
+        return newKey.toString();
+    }
 }
