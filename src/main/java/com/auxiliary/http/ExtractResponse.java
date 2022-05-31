2b07db861799ba4d6688f41d57d98eda44e6dd8d
彭宇琦 <465615774@qq.com>
2022-05-31 08:23:57 +0800

    添加搜词常量

--- src/main/java/com/auxiliary/http/ExtractResponse.java
+++ src/main/java/com/auxiliary/http/ExtractResponse.java
@@ -24,6 +24,30 @@ import java.util.Set;
  */
 public interface ExtractResponse {
     /**
+     * 定义元素名称中的“.”符号的转义符号，调用该符号后，其变量名将作为整体看待，不会被切割
+     * <p>
+     * 例如，“valus.s”在识别时会被当成两个元素名称，而“value\.s”则将作为单个变量名称使用
+     * </p>
+     */
+    public static final String SEPARATE_TRANSFERRED_MEANING_REGEX = "\\\\.";
+    /**
+     * 定义元素名称分隔
+     */
+    public static final String SEPARATE_SYMBOL = ".";
+    /**
+     * 定义切分元素名称的正则符号
+     */
+    public static final String SEPARATE_SPLIT_REGEX = "\\.";
+    /**
+     * 定义查找数组元素起始标志符号
+     */
+    public static final String ARRAY_START_SYMBOL = "[";
+    /**
+     * 定义查找数组元素结束标志符号
+     */
+    public static final String ARRAY_END_SYMBOL = "]";
+
+    /**
      * 该方法用于指定接口的报文提词内容json串集合
      * <p>
      * json串的键值可通过静态属性获取，当没有断言内容时，则json中的键都为空值
