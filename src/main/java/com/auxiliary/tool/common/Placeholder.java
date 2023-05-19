57524680b38ded00585bf7a5d094d422d8cece6d
彭宇琦 <465615774@qq.com>
2023-05-19 16:28:18 +0800

    添加部分方法

--- src/main/java/com/auxiliary/tool/common/Placeholder.java
+++ src/main/java/com/auxiliary/tool/common/Placeholder.java
@@ -1,7 +1,10 @@
 package com.auxiliary.tool.common;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,6 +13,7 @@ import com.auxiliary.datadriven.DataFunction;
 import com.auxiliary.datadriven.Functions;
 import com.auxiliary.tool.regex.ConstType;
 import com.auxiliary.tool.regex.RegexType;
+import com.google.common.base.Objects;
 
 /**
  * <p>
@@ -45,11 +49,11 @@ public class Placeholder {
     private String endSign = "";
 
     /**
-     * 递归替换的深度
+     * 循环替换的深度，默认10次
      * 
      * @since autest 4.2.0
      */
-    private int replaceDepth = 5;
+    private int replaceDepth = 10;
 
     /**
      * 存储需要替换的占位符以及占位符相关的替换公式
@@ -89,9 +93,20 @@ public class Placeholder {
     }
 
     /**
+     * 该方法用于返回占位符的起始与结束标志，在数组中，第一个元素表示起始标志，第二个元素表示结束标志
+     * 
+     * @return 占位符的起始与结束标志
+     * @since autest 4.2.0
+     */
+    public String[] getPlaceholderSign() {
+        return new String[] { startSign, endSign };
+    }
+
+    /**
      * 该方法用于设置替换占位符的深度（替换次数），避免嵌套替换占位符导致死循环
      * <p>
-     * 例如，设置占位符起始与结束标志均为“#”符号，存储占位符“1”的替换内容为“#2#”，存储占位符“2”的替换内容为“#3#”；则当需要替换的内容为“内容：#1#”时：
+     * 该数值主要用在词语嵌套替换时会生效，非嵌套替换或无法被替换的占位符不受该数值的影响。例如，设置占位符起始与结束标志均为“#”符号，
+     * 存储占位符“1”的替换内容为“#2#”，存储占位符“2”的替换内容为“#3#”；则当需要替换的内容为“内容：#1#”时：
      * <ol>
      * <li>第一次替换后，文本变为“内容：#2#”</li>
      * <li>第二次替换后，文本变为“内容：#3#”</li>
@@ -100,7 +115,7 @@ public class Placeholder {
      * 根据以上替换次数，则说明文本的替换深度为3层（3次替换），若设置深度为2，则替换后的文本将返回“内容：#3#”
      * </p>
      * <p>
-     * <b>注意：</b>深度必须设置大于等于0次（0次则表示不替换），若设置的深度为负数，则不会生效，将沿用上一次设置的值（初次设置为负数时，则使用默认次数，默认次数为5次）
+     * <b>注意：</b>深度必须设置大于等于0次（0次则表示不替换），若设置的深度为负数，则不会生效，将沿用上一次设置的值（初次设置为负数时，则使用默认次数，默认次数为10次）
      * </ul>
      * </p>
      * 
@@ -115,6 +130,7 @@ public class Placeholder {
 
         return this;
     }
+
 
     /**
      * 用于添加待替换的词语及相应的替换方法
@@ -139,7 +155,10 @@ public class Placeholder {
      * </pre></code>
      * </p>
      * <p>
-     * 部分定义方法可调用工具类{@link Functions}类获取
+     * 部分定义方法可调用工具类{@link Functions}类获取，以其中一个方法为例，其传参方法为如下： <code><pre>
+     * {@link DataDriverFunction} driverFunction = {@link Functions#randomCarId()};
+     * addReplaceFunction({@link DataDriverFunction#getRegex()}, {@link DataDriverFunction#getFunction()});
+     * </pre></code>
      * </p>
      * <p>
      * <b>注意：</b>为加快若传入的内容不包含正则相关的内容，则仍按照词语进行存储，其规则可参考枚举类{@link RegexType#REGEX_REGEX}
@@ -149,20 +168,54 @@ public class Placeholder {
      * @return 类本身
      * @since autest 4.2.0
      */
-    public Placeholder addReplaceFunction(DataDriverFunction functions) {
-        if (functions == null) {
+    public Placeholder addReplaceFunction(String regex, DataFunction function) {
+        if (regex == null || regex.isEmpty()) {
             return this;
         }
 
         // 获取正则内容，判断传入的规则中是否包含正则字符，若不包含，则仍按照词语进行存储
-        String regex = functions.getRegex();
         if (regex.matches(RegexType.REGEX_REGEX.getRegex())) {
-            replaceFunctionMap.put(regex, functions.getFunction());
+            replaceFunctionMap.put(regex, function);
         } else {
-            addReplaceWord(regex, functions.getFunction().apply(regex));
+            addReplaceWord(regex, function.apply(regex));
         }
 
         return this;
+    }
+
+    /**
+     * 该方法用于添加待替换的词语及相应的替换方法集合
+     * <p>
+     * 集合的键为占位符的词语，值为替换的方法，可参考{@link #addReplaceFunction(String, DataFunction)}的方法添加
+     * </p>
+     * 
+     * @param functionMap 替换方法集合
+     * @return 类本身
+     * @since autest 4.2.0
+     */
+    public Placeholder addReplaceFunction(Map<String, DataFunction> functionMap) {
+        functionMap.forEach(this::addReplaceFunction);
+        return this;
+    }
+
+    /**
+     * 该方法用于返回添加待替换的词语及相应的替换方法集合
+     * <p>
+     * 集合的键为占位符的词语，值为替换的方法
+     * </p>
+     * 
+     * @return 替换方法集合
+     * @since autest 4.2.0
+     */
+    public Map<String, DataFunction> getReplaceFunction() {
+        return new HashMap<>(replaceFunctionMap);
+    }
+
+    public Map<String, DataFunction> clearReplaceFunction() {
+        Map<String, DataFunction> replaceFunctionMap = getReplaceFunction();
+        replaceFunctionMap.clear();
+
+        return replaceFunctionMap;
     }
 
     /**
@@ -175,16 +228,60 @@ public class Placeholder {
      */
     public Placeholder addReplaceWord(String word, String replaceWord) {
         if (word != null && !word.isEmpty()) {
-            replaceWordMap.put(word, replaceWord);
+            replaceWordMap.put(word, Optional.ofNullable(replaceWord).orElse(""));
         }
 
         return this;
     }
 
     /**
+     * 该方法用于添加一组待替换的词语及相应的替换内容
+     * 
+     * @param wordMap 占位符词语集合
+     * @return 类本身
+     * @since autest 4.2.0
+     */
+    public Placeholder addReplaceWord(Map<String, String> wordMap) {
+        wordMap.forEach(this::addReplaceWord);
+        return this;
+    }
+
+    /**
+     * 该方法用于返回当前存储的替换词语集合，修改返回的集合不影响类中存储的集合内容
+     * <p>
+     * 返回的集合中，键为占位符词语，值为替换的内容
+     * </p>
+     * 
+     * @return 替换词语集合
+     * @since autest 4.2.0
+     */
+    public Map<String, String> getReplaceWordMap() {
+        return new HashMap<>(replaceWordMap);
+    }
+
+    /**
+     * 该方法用于清空存储的替换词语集合，并返回原始存储的内容
+     * 
+     * @return 替换词语集合
+     * @since autest 4.2.0
+     * @see #getReplaceWordMap()
+     */
+    public Map<String, String> clearReplaceWordMap() {
+        Map<String, String> replaceWordMap = getReplaceWordMap();
+        replaceWordMap.clear();
+
+        return replaceWordMap;
+    }
+
+    /**
      * 该方法用于对需要替换的词语进行转换，输出该词语对应的替换后的内容
      * <p>
-     * <b>注意：</b>传入的词语不能包含占位符的前后标志
+     * <b>注意：</b>
+     * <ol>
+     * <li>传入的词语不能包含占位符的前后标志</li>
+     * <li>词语无法替换，则返回原始词语</li>
+     * <li>传入的词语为空串或null时，则按照空串进行返回</li>
+     * </ol>
      * </p>
      * 
      * @param word 需要替换的词语
@@ -192,6 +289,10 @@ public class Placeholder {
      * @since autest 4.2.0
      */
     public String replaceWord(String word) {
+        if (word == null || word.isEmpty()) {
+            return "";
+        }
+
         // 替换词语的方法：
         // 1. 先判断当前的词语是否直接可在替换词语集合中找到，即词语替换
         // 2. 若不存在词语替换，则按照正则表达式再次判断，进行替换
@@ -225,7 +326,11 @@ public class Placeholder {
         }
         
         // 根据替换深度，循环对词语进行替换；若其内不包含需要替换的内容时，则结束循环
+        // 存储上一次查找的占位符词语
+        Set<String> doneReplaceWordSet = new HashSet<>();
         for (int i = 0; i < replaceDepth && text.matches(String.format(".*%s.*", functionSign)); i++) {
+            // 存储当前查找的占位符词语
+            Set<String> nowReplaceWordSet = new HashSet<>();
             // 通过函数标志对文本中的函数或方法进行提取
             Matcher m = Pattern.compile(functionSign).matcher(text);
             while (m.find()) {
@@ -233,8 +338,26 @@ public class Placeholder {
                 String signKey = m.group();
                 String key = signKey.substring(startSign.length(), signKey.length() - signKey.indexOf(endSign));
                 // 调用替换词语的方法，对占位符进行替换
-                text = text.replaceAll(DisposeCodeUtils.disposeRegexSpecialSymbol(signKey), replaceWord(key));
+                String replaceKey = replaceWord(key);
+                // 判断调用替换方法后，其返回的词语与替换词语是否一致，若不一致，则进行替换，否则，将不做处理
+                if (!Objects.equal(key, replaceKey)) {
+                    text = text.replaceAll(DisposeCodeUtils.disposeRegexSpecialSymbol(signKey), replaceKey);
+                }
+                // 添加当前查找到的占位符词语
+                nowReplaceWordSet.add(key);
             }
+
+            // 两层判断：
+            // 1. 判断当前是否还存在替换词语
+            // 2. 判断当前存储的词语是否与上一次存储的词语一致，一致则说明当前词语无法被替换
+            // 若满足其中一个条件，则结束当前的循环，避免执行无用的次数
+            if (nowReplaceWordSet.isEmpty() || nowReplaceWordSet.equals(doneReplaceWordSet)) {
+                break;
+            }
+
+            // 若不满足上述条件，则清空上一次存储的占位符词语，并存储当前的词语
+            doneReplaceWordSet.clear();
+            doneReplaceWordSet.addAll(nowReplaceWordSet);
         }
 
         return text;
